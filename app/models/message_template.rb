da5ae9d636253064898e31c0c1037faf0cba3ee3
Praveen Angyan <angyan@usc.edu>
2016-02-09 00:10:35 +0000

    Adding in feature to standardize variables used in message templates (store content of message templates downcased and stripped of white space)

--- app/models/message_template.rb
+++ app/models/message_template.rb
@@ -1,4 +1,22 @@
 class MessageTemplate < ActiveRecord::Base
   validates :content, presence: true
   validates :platform, presence: true
+
+  STANDARD_VARIABLES = [/{\s*pi_first_name\s*}/i, /{\s*pi_last_name\s*}/i, /{\s*disease\s*}/i]
+    
+  def content=(content)
+    return if content.blank?
+
+    # Does the content of the message template contain any of the standard variables?
+    STANDARD_VARIABLES.each do |variable|
+      matches = content.scan(variable)
+      
+      if matches.size > 0
+        puts "Match: #{matches[0]}"
+        content.gsub!(matches[0], matches[0].downcase.gsub(/\s/, ''))
+      end
+    end
+
+    write_attribute(:content, content)
+  end
 end
--- spec/models/message_template_spec.rb
+++ spec/models/message_template_spec.rb
@@ -3,9 +3,59 @@ require 'rails_helper'
 RSpec.describe MessageTemplate do
   it { is_expected.to validate_presence_of :content }
   it { is_expected.to validate_presence_of :platform }
+  
+  context 'containing variables' do
+    it 'lowercases the pi_first_name variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a {PI_first_name} variable')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {pi_first_name} variable')
+    end
+
+    it 'lowercases the pi_last_name variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a {pi_last_NAME} variable')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {pi_last_name} variable')
+    end
+
+    it 'lowercases the disease variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a {Disease} variable')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {disease} variable')
+    end
+    
+    it 'strips out whitespace in the pi_first_name variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a { PI_first_name } variable with whitespace')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {pi_first_name} variable with whitespace')
+    end
+
+    it 'strips out whitespace in the pi_last_name variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a {pi_last_NAME  } variable with whitespace')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {pi_last_name} variable with whitespace')
+    end
+
+    it 'strips out whitespace in the disease variable' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing a {  Disease} variable with whitespace')
+      
+      expect(message_template.content).to eq('This is a message_template containing a {disease} variable with whitespace')
+    end
+    
+    it 'strips out whitespace and downcases multiple and different variables' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing {  Disease}, {PI_first_name }, {PI_LAST_NAME  } variables')
+
+      expect(message_template.content).to eq('This is a message_template containing {disease}, {pi_first_name}, {pi_last_name} variables')
+    end
+    
+    it 'strips out whitespace and downcases multiple and duplicate variables' do
+      message_template = MessageTemplate.new(content: 'This is a message_template containing {  Disease}, {PI_first_name }, {PI_LAST_NAME  } variables which are duplicated here: {  Disease}, {PI_first_name }, {PI_LAST_NAME  }')
+
+      expect(message_template.content).to eq('This is a message_template containing {disease}, {pi_first_name}, {pi_last_name} variables which are duplicated here: {disease}, {pi_first_name}, {pi_last_name}')
+    end
+  end
+  
 
   # it { is_expected.to have_many :messages }
-
 #   it 'saves the content as a string' do
 #     message_template = MessageTemplate.new(:initial_id => "1", :platform => "twitter", :message_type => "awareness", :content => 'Some content')
 
