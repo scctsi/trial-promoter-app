6f966baff45757ef2973c14f86676b86e2c7f60a
Praveen Angyan <angyan@usc.edu>
2016-08-30 23:24:22 +0000

    Use current scope for Campaign in Promoter

--- lib/promoter.rb
+++ lib/promoter.rb
@@ -1,6 +1,6 @@
 class Promoter
   def pick_clinical_trials
-    current_campaign_ids = Campaign.where('start_date <= ? or start_date is null', DateTime.now).pluck(:id)
+    current_campaign_ids = Campaign.current.pluck(:id)
     clinical_trials = ClinicalTrial.joins('join campaigns_clinical_trials on clinical_trials.id = campaigns_clinical_trials.clinical_trial_id').where('campaigns_clinical_trials.campaign_id is not null and campaigns_clinical_trials.campaign_id in ' + '(' + current_campaign_ids.join(',') + ')').order(last_promoted_at: :asc)
 
     return clinical_trials
