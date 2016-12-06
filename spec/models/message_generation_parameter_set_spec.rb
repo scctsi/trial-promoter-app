19622eb05216c0d1033261d5f1059bdcf0fe7f2e
Praveen Angyan <angyan@usc.edu>
2016-12-06 00:19:35 +0000

    Refactor MessageSetGenerator to MessageFactory

--- spec/models/message_generation_parameter_set_spec.rb
+++ spec/models/message_generation_parameter_set_spec.rb
@@ -60,30 +60,30 @@ describe MessageGenerationParameterSet do
     expect(message_generation_parameter_set.image_present_choices).to eq(['with', 'without'])
   end
 
-  describe 'calculating the number of generated messages' do
+  describe 'number of generated messages' do
     before do
       experiment = create(:experiment)
       @websites = create(:website, experiment_list: experiment.to_param)
       @message_templates = create_list(:message_template, 5, experiment_list: experiment.to_param)
     end
     
-    it 'is correct when the parameters include one website, five message templates, 1 social network (equal distribution), 1 medium (equal distribution), with images (equal distribution), for 10 days and 3 messages per network per day' do
-      message_generation_parameter_set = MessageGenerationParameterSet.new(
-        social_network_choices: ['facebook'],
-        social_network_distribution: :equal,
-        medium_choices: ['ad'],
-        medium_distribution: :equal,
-        image_present_choices: ['with'],
-        image_present_distribution: :equal,
-        period_in_days: 10,
-        number_of_messages_per_social_network: 3
-      )
+    it 'is calculated correctly when the parameters include one website, five message templates, 1 social network (equal distribution), 1 medium (equal distribution), with images (equal distribution), for 10 days and 3 messages per network per day' do
+      message_generation_parameter_set = MessageGenerationParameterSet.new do |m|
+        m.social_network_choices = ['facebook']
+        m.social_network_distribution = :equal
+        m.medium_choices = ['ad']
+        m.medium_distribution = :equal
+        m.image_present_choices = ['with']
+        m.image_present_distribution = :equal
+        m.period_in_days = 10
+        m.number_of_messages_per_social_network = 3
+      end
       
       # Number of social networks (1) * Number of mediums (1) * Number of image present choices (1) * Period in days(10) * Number of messages per social network (3)
       expect(message_generation_parameter_set.expected_generated_message_count).to eq(1 * 1 * 1 * 10 * 3)
     end
 
-    it 'is correct when the parameters include one website, five message templates, 3 social networks (equal distribution), 2 mediums (equal distribution), with and without images (equal distribution), for 10 days and 3 messages per network per day' do
+    it 'is calculated correctly when the parameters include one website, five message templates, 3 social networks (equal distribution), 2 mediums (equal distribution), with and without images (equal distribution), for 10 days and 3 messages per network per day' do
       message_generation_parameter_set = MessageGenerationParameterSet.new(
         social_network_choices: ['facebook', 'twitter', 'instagram'],
         social_network_distribution: :equal,
