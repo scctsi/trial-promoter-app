46146ed071475390dff98ef113c4a314ea4feacb
Praveen Angyan <angyan@usc.edu>
2017-01-04 23:06:11 +0000

    Implement call to Google API gem for analytics data

--- lib/google_analytics_client.rb
+++ lib/google_analytics_client.rb
@@ -1,20 +1,29 @@
 require 'google/apis/analytics_v3'
 
 class GoogleAnalyticsClient
+  DEFAULT_METRICS = 'ga:users,ga:sessions,ga:sessionDuration,ga:timeOnPage,ga:avgSessionDuration,ga:avgTimeOnPage,ga:pageviews,ga:exits'
+  DEFAULT_DIMENSIONS = 'ga:campaign,ga:sourceMedium,ga:adContent'
+
   attr_reader :profile_id, :scopes, :google_auth_json_file, :service
 
   def initialize(profile_id)
     @profile_id = profile_id
     @scopes = ['https://www.googleapis.com/auth/analytics.readonly']
     @google_auth_json_file = StringIO.new(Setting[:google_auth_json_file])
+
+    # Authenticate and create a service object
+    Google::Apis::ClientOptions.default.application_name = 'Trial Promoter'
+    Google::Apis::ClientOptions.default.application_version = '1.0.0'
     auth = Google::Auth::ServiceAccountCredentials.make_creds(json_key_io: @google_auth_json_file, scope: @scopes)
     @service = Google::Apis::AnalyticsV3::AnalyticsService.new
     @service.authorization = auth
-    Google::Apis::ClientOptions.default.application_name = 'Trial Promoter'
-    Google::Apis::ClientOptions.default.application_version = '1.0.0'
   end
   
   def table_id
     "ga:#{profile_id}"
+  end
+
+  def get_data(start_date, end_date, metric_list = DEFAULT_METRICS, dimension_list = DEFAULT_DIMENSIONS)  
+    @service.get_ga_data(table_id, start_date, end_date, metric_list, dimensions: dimension_list, max_results: 100000)
   end
 end
--- spec/lib/google_analytics_client_spec.rb
+++ spec/lib/google_analytics_client_spec.rb
@@ -4,23 +4,43 @@ RSpec.describe GoogleAnalyticsClient do
   before do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:google_auth_json_file).and_return(secrets['google_auth_json_file'])
-    @google_analytics_client = GoogleAnalyticsClient.new('92952002')
   end
   
   describe "(development only tests)", :development_only_tests => true do
     it 'can be initialized given a profiles ID (Analytics view ID)' do
       google_analytics_client = GoogleAnalyticsClient.new('100')
-      
+
       expect(google_analytics_client.profile_id).to eq('100')
       expect(google_analytics_client.table_id).to eq('ga:100') 
       expect(google_analytics_client.scopes).to eq(['https://www.googleapis.com/auth/analytics.readonly'])
       expect(google_analytics_client.google_auth_json_file).to be_a(StringIO)
+      # Also check that it creates an authenticated Analytics service object instance'
+      expect(google_analytics_client.service).not_to be_nil
+      expect(google_analytics_client.service.client_options.application_name).to eq('Trial Promoter')
+      expect(google_analytics_client.service.client_options.application_version).to eq('1.0.0')
     end
     
-    it 'creates an authenticated Analytics service instance' do
-      expect(@google_analytics_client.service).not_to be_nil
-      expect(@google_analytics_client.service.client_options.application_name).to eq('Trial Promoter')
-      expect(@google_analytics_client.service.client_options.application_version).to eq('1.0.0')
+    it 'defines a default set of metrics and dimensions' do
+      expect(GoogleAnalyticsClient::DEFAULT_METRICS).to eq('ga:users,ga:sessions,ga:sessionDuration,ga:timeOnPage,ga:avgSessionDuration,ga:avgTimeOnPage,ga:pageviews,ga:exits')
+      expect(GoogleAnalyticsClient::DEFAULT_DIMENSIONS).to eq('ga:campaign,ga:sourceMedium,ga:adContent')
+    end
+    
+    it 'gets analytics data given a start and end date (using the default metrics and dimensions)' do
+      google_analytics_client = GoogleAnalyticsClient.new('92952002')
+      allow(google_analytics_client.service).to receive(:get_ga_data)
+
+      google_analytics_client.get_data('2016-01-01', '2016-01-02')
+      
+      expect(google_analytics_client.service).to have_received(:get_ga_data).with(google_analytics_client.table_id, '2016-01-01', '2016-01-02', GoogleAnalyticsClient::DEFAULT_METRICS, dimensions: GoogleAnalyticsClient::DEFAULT_DIMENSIONS, max_results: 100000)
+    end
+
+    it 'gets analytics data given a start, end date, metric and dimension list' do
+      google_analytics_client = GoogleAnalyticsClient.new('92952002')
+      allow(google_analytics_client.service).to receive(:get_ga_data)
+
+      google_analytics_client.get_data('2016-01-01', '2016-01-02', 'gaSessions,gaUsers', 'gaCampaign,gaSourceMedium')
+      
+      expect(google_analytics_client.service).to have_received(:get_ga_data).with(google_analytics_client.table_id, '2016-01-01', '2016-01-02', 'gaSessions,gaUsers', dimensions: 'gaCampaign,gaSourceMedium', max_results: 100000)
     end
   end
 end
