258644614f402bfe81c06da1b2cd55ff253b0ff3
Praveen Angyan <angyan@usc.edu>
2017-01-10 22:27:03 +0000

    Implement test for creating an adset

--- spec/lib/facebook_client_spec.rb
+++ spec/lib/facebook_client_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe FacebookClient do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:facebook_access_token).and_return(secrets['facebook_access_token'])
     @facebook_client = FacebookClient.new
+    @account = nil
+    
+    VCR.use_cassette 'facebook_client/test_setup' do
+      accounts = @facebook_client.get_accounts
+      @account = accounts.first
+    end
   end
   
   describe "(development only tests)", :development_only_tests => true do
@@ -32,8 +38,7 @@ RSpec.describe FacebookClient do
       campaigns = nil
 
       VCR.use_cassette 'facebook_client/get_campaigns' do
-        account = @facebook_client.get_accounts.first
-        campaigns = @facebook_client.get_campaigns(account)
+        campaigns = @facebook_client.get_campaigns(@account)
       end
       
       expect(campaigns.count).to be >= 1
@@ -43,8 +48,7 @@ RSpec.describe FacebookClient do
       campaign_exists = false
       
       VCR.use_cassette 'facebook_client/campaign_exists?' do
-        accounts = @facebook_client.get_accounts
-        campaign_exists = @facebook_client.campaign_exists?(accounts.first, "Test")
+        campaign_exists = @facebook_client.campaign_exists?(@account, "Test")
       end
       
       expect(campaign_exists).to be true
@@ -53,33 +57,31 @@ RSpec.describe FacebookClient do
     # These tests write to our ad account on Facebook
     # So all these tests use mocks for testing, thus preventing any real write calls to our ad account
     it 'creates a campaign given a name and objective if the campaign does not exist' do
-      account = nil
       allow(@facebook_client).to receive(:campaign_exists?).and_return(false)
       experiment = create(:experiment, name: 'tcors')
       VCR.use_cassette 'facebook_client/create_campaign_if_it_does_not_exist' do
-        accounts = @facebook_client.get_accounts
-        account = accounts.first
-        allow(account).to receive(:create_campaign)
+        allow(@account).to receive(:create_campaign)
       end
 
-      @facebook_client.create_campaign(account, experiment.to_param)
+      @facebook_client.create_campaign(@account, experiment.to_param)
       
-      expect(account).to have_received(:create_campaign).with({name: experiment.to_param, objective: 'LINK_CLICKS'})
+      expect(@account).to have_received(:create_campaign).with({name: experiment.to_param, objective: 'LINK_CLICKS'})
     end
 
     it 'does not create a campaign if the campaign already exists' do
-      account = nil
       allow(@facebook_client).to receive(:campaign_exists?).and_return(true)
       experiment = create(:experiment, name: 'tcors')
       VCR.use_cassette 'facebook_client/create_campaign_if_it_does_not_exist' do
-        accounts = @facebook_client.get_accounts
-        account = accounts.first
-        allow(account).to receive(:create_campaign)
+        allow(@account).to receive(:create_campaign)
       end
 
-      @facebook_client.create_campaign(account, experiment.to_param)
+      @facebook_client.create_campaign(@account, experiment.to_param)
       
-      expect(account).not_to have_received(:create_campaign)
+      expect(@account).not_to have_received(:create_campaign)
+    end
+    
+    it 'creates an adset' do
+      
     end
   end
 end
