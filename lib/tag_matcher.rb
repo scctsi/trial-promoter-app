5a4456043c89c24362b9a2391e4a8ad73e2d63c0
Praveen Angyan <angyan@usc.edu>
2017-01-11 23:11:10 +0000

    Add find and matching to TagMatcher

--- lib/tag_matcher.rb
+++ lib/tag_matcher.rb
@@ -1,2 +1,18 @@
 class TagMatcher
+  def find(class_with_tags, tags)
+    class_with_tags.send(:tagged_with, tags)
+  end
+  
+  def match(class_with_tags, tagged_instance)
+    matches = []
+    
+    (1..tagged_instance.tag_list.length).each do |combination_length|
+      tagged_instance.tag_list.combination(combination_length).to_a.each do |tag_combination|
+        found_instances = class_with_tags.send(:tagged_with, tag_combination, :match_all => true)
+        matches.concat(found_instances.to_a) if found_instances.length > 0
+      end
+    end
+
+    matches.uniq!
+  end
 end
