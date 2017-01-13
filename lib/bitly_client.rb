048dba8c6ef9ff59bc7f482dcfdb9b84884ac9fd
Praveen Angyan <angyan@usc.edu>
2017-01-12 16:56:11 -0800

    Implement bitly integration (#15)
    * Add initial implementation of Bitly client
    
    * Add simple bit.ly integration

---
+++ lib/bitly_client.rb
@@ -0,0 +1,18 @@
+class BitlyClient
+  def initialize
+    Bitly.use_api_version_3
+
+    Bitly.configure do |config|
+      config.api_version = 3
+      config.access_token = Setting[:bitly_access_token]
+    end
+  end
+  
+  def shorten(url)
+    Bitly.client.shorten(url).short_url
+  end
+  
+  def expand(bitly_url)
+    Bitly.client.expand(bitly_url).long_url
+  end
+end
