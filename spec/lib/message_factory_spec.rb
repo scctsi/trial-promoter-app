04e1a84c0c381985097ae6173aa27b340b441147
Praveen Angyan <angyan@usc.edu>
2017-01-13 21:20:18 +0000

    Use TagMatcher during message generation

--- spec/lib/message_factory_spec.rb
+++ spec/lib/message_factory_spec.rb
@@ -3,13 +3,30 @@ require 'rails_helper'
 RSpec.describe MessageFactory do
   before do
     @experiment = create(:experiment)
-    create(:website, experiment_list: @experiment.to_param)
+    tag_lists = [['tag-1'], ['tag-4', 'tag-5', 'tag-6'], ['tag-2', 'tag-3', 'tag-4']]
     TrialPromoter::SUPPORTED_NETWORKS.each do |social_network|
-      create_list(:message_template, 5, platform: social_network, experiment_list: @experiment.to_param)
+      # Randomly tag each message template with a set of tags taken from tag_lists
+      create_list(:message_template, 5, platform: social_network, experiment_list: @experiment.to_param, tag_list: tag_lists.sample)
     end
-    @message_factory = MessageFactory.new
+    # Set up tag matching
+    @tag_matcher = TagMatcher.new
+    @image_sets_matching_tag_sets = [create_list(:image, 1), create_list(:image, 2), create_list(:image, 1)]
+    allow(@tag_matcher).to receive(:match).with(Image, tag_lists[0]).and_return(@image_sets_matching_tag_sets[0])
+    allow(@tag_matcher).to receive(:match).with(Image, tag_lists[1]).and_return(@image_sets_matching_tag_sets[1])
+    allow(@tag_matcher).to receive(:match).with(Image, tag_lists[2]).and_return(@image_sets_matching_tag_sets[2])
+    @website_sets_matching_tag_sets = [create_list(:website, 2), create_list(:website, 1), create_list(:website, 3)]
+    allow(@tag_matcher).to receive(:match).with(Website, tag_lists[0]).and_return(@website_sets_matching_tag_sets[0])
+    allow(@tag_matcher).to receive(:match).with(Website, tag_lists[1]).and_return(@website_sets_matching_tag_sets[1])
+    allow(@tag_matcher).to receive(:match).with(Website, tag_lists[2]).and_return(@website_sets_matching_tag_sets[2])
+    @message_factory = MessageFactory.new(@tag_matcher)
   end
 
+  it 'can be initialized with a tag matcher' do
+    @message_factory = MessageFactory.new(@tag_matcher)
+    
+    expect(@message_factory.tag_matcher).to eq(@tag_matcher)
+  end
+  
   it 'creates a set of messages for one website, five message templates, 1 social network (equal distribution), 1 medium (equal distribution), with images (equal distribution), for 10 days and 3 messages per network per day' do
     message_generation_parameter_set = MessageGenerationParameterSet.new do |m|
       m.social_network_choices = [:facebook]
@@ -47,7 +64,7 @@ RSpec.describe MessageFactory do
     messages = @experiment.messages
 
     expect(messages.count).to eq(message_generation_parameter_set.expected_generated_message_count)
-    messages.each do |message|
+    messages.all.each do |message|
       expect(message.message_generating).to eq(@experiment)
     end
     # Are the messages equally distributed across social networks?
@@ -56,6 +73,16 @@ RSpec.describe MessageFactory do
     expect_equal_distribution(messages.group_by { |message| message.medium })
     # Are the messages equally distributed across image present choices?
     expect_equal_distribution(messages.group_by { |message| message.image_present })
+    
+    # Tag matching
+    # Do the websites selected for each message belong to the set of images matched to the message template?
+    messages.all.each do |message|
+      expect(@tag_matcher.match(Website, message.message_template.tag_list)).to include(message.promotable)
+    end
+    # Do the images selected for each message belong to the set of images matched to the message template?
+    messages.all.each do |message|
+      expect(@tag_matcher.match(Image, message.message_template.tag_list)).to include(message.image) if message.image_present
+    end
   end
 
   it 'recreates the messages each time' do
@@ -77,7 +104,7 @@ RSpec.describe MessageFactory do
     messages = Message.all
     expect(messages.count).to eq(message_generation_parameter_set.expected_generated_message_count)
   end
-  
+
   def expect_equal_distribution(grouped_messages)
     keys = grouped_messages.keys
     (0...(keys.count - 1)).each do |index|
