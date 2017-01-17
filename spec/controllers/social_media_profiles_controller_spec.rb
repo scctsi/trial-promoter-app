b14525b80f9ef0bb9156933860e378cf755fc80d
Praveen Angyan <angyan@usc.edu>
2017-01-17 23:03:52 +0000

    Rename Buffer to BufferClient

--- spec/controllers/social_media_profiles_controller_spec.rb
+++ spec/controllers/social_media_profiles_controller_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe SocialMediaProfilesController, type: :controller do
 
   describe "GET #sync_with_buffer" do
     before do
-      allow(Buffer).to receive(:get_social_media_profiles)
+      allow(BufferClient).to receive(:get_social_media_profiles)
       get :sync_with_buffer
     end
 
     it 'calls the Buffer library to sync up the social media profiles' do
-      expect(Buffer).to have_received(:get_social_media_profiles)
+      expect(BufferClient).to have_received(:get_social_media_profiles)
     end
     
     it 'redirects to the Settings index page' do
