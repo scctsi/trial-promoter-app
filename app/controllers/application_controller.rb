0e4c47b804d710ee3bfc837f5c8d0c802c445e75
Alicia MacLennan <aflyingfemme@gmail.com>
2017-01-17 09:11:02 -0800

    Add setting policy/spec; change favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
-  after_action :verify_authorized
+  after_action :verify_authorized, unless: :devise_controller?
 end
 
 
