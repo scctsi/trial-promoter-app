16197bf26deeb776e9476a4d61755d8d564871ce
Praveen Angyan <angyan@usc.edu>
2017-01-17 22:57:56 +0000

    Setup Delayed Job

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Set the Active Job queueing backend to Delayed Job
+    config.active_job.queue_adapter = :delayed_job
   end
 end
