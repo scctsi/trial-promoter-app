7c7cd1871e2486dc050710562447bd02fad63352
Praveen Angyan <angyan@usc.edu>
2017-02-03 17:07:13 +0000

    Fix app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -230,64 +230,6 @@ $(document).ready(function() {
     });
   }
   
-  function setUpImageTagging() {
-    var $imageSelectors = $('.image-selector');
-    
-    // Set up tag editor
-    $('#image-tags').selectize({
-      delimiter: ',',
-      persist: false,
-      create: true
-    });
-    
-    // Set up all checkboxes
-    $imageSelectors.checkbox();
-    $imageSelectors.checkbox('attach events', '#select-all-images-button', 'check');
-    $imageSelectors.checkbox('attach events', '#deselect-all-images-button', 'uncheck');
-
-    // Set up AJAX call to replace tags on selected images with contents of tag editor
-    var selectedImageIds = [];
-    var tags = '';
-    $("#add-image-tags-button").on('click', function() {
-      selectedImageIds = [];
-      
-      $imageSelectors.each(function() {
-        if ($(this).find('input').is(':checked')) {
-          selectedImageIds.push($(this).data('image-id'));
-          tags = $('#image-tags').val();
-        };
-      })
-      
-      $.ajax({
-        url : '/images/tag_images',
-        type: 'POST',
-        data: {image_ids: selectedImageIds, tags: tags},
-        dataType: 'json',
-        success: function(retdata) {
-          var imageTagCells = [];
-          
-          $imageSelectors.each(function() {
-            if ($(this).find('input').is(':checked')) {
-              imageTagCells.push($(this).parent().parent().find('td.image-tag'));
-            };
-          })
-
-          imageTagCells.forEach(function(imageTagCell) {
-            var tagHtml = '';
-            var splitTags = tags.split(',');
-
-            splitTags.forEach(function(tag) {
-              tagHtml += '<a class="ui small tag label">' + tag + '</a>';  
-            });
-            imageTagCell.html(tagHtml);
-          })
-        }
-      });
-
-      return false;
-    });
-  }
-
   function setUpPusherChannels() {
     var pusher = new Pusher('645d88fef1ee61febc2d'); // uses your APP KEY
     var channel = pusher.subscribe('progress');
