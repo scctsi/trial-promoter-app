8233e04ca501c5951b94124172f9268e5fa08195
Praveen Angyan <angyan@usc.edu>
2017-02-02 17:09:10 -0800

    Restrict selectable tags for images (#40)
    * Show improved tags across all views
    
    * Restrict tags to message template tags

--- spec/lib/tag_matcher_spec.rb
+++ spec/lib/tag_matcher_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe TagMatcher do
   before do
     @tag_matcher = TagMatcher.new
+    @websites = create_list(:website, 3)
   end
   
   it 'finds an instance of a specified class that contains all the specified tags' do
-    websites = create_list(:website, 3)
-    websites[0].tag_list.add('tag-1')
-    websites[1].tag_list.add('tag-1', 'tag-2')
-    websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3')
-    websites.each { |website| website.save }
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-1', 'tag-2')
+    @websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3')
+    @websites.each { |website| website.save }
     
     tagged_websites = @tag_matcher.find(Website, ['tag-1', 'tag-2', 'tag-3'])
     
@@ -19,11 +19,10 @@ RSpec.describe TagMatcher do
   end
 
   it 'finds all instances of a specified class that contains all the specified tags' do
-    websites = create_list(:website, 3)
-    websites[0].tag_list.add('tag-1')
-    websites[1].tag_list.add('tag-1', 'tag-2', 'tag-3')
-    websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3')
-    websites.each { |website| website.save }
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-1', 'tag-2', 'tag-3')
+    @websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3')
+    @websites.each { |website| website.save }
     
     tagged_websites = @tag_matcher.find(Website, ['tag-1', 'tag-2', 'tag-3'])
     
@@ -32,11 +31,10 @@ RSpec.describe TagMatcher do
   end
 
   it 'finds all instances of a specified class that contains all the specified tags and include additional tags' do
-    websites = create_list(:website, 3)
-    websites[0].tag_list.add('tag-1')
-    websites[1].tag_list.add('tag-1', 'tag-2', 'tag-3')
-    websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
-    websites.each { |website| website.save }
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-1', 'tag-2', 'tag-3')
+    @websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
+    @websites.each { |website| website.save }
     
     tagged_websites = @tag_matcher.find(Website, ['tag-1', 'tag-2', 'tag-3'])
     
@@ -45,11 +43,10 @@ RSpec.describe TagMatcher do
   end
 
   it 'returns an empty array if no instances of the specified class contain all the tags' do
-    websites = create_list(:website, 3)
-    websites[0].tag_list.add('tag-1')
-    websites[1].tag_list.add('tag-1', 'tag-2')
-    websites[2].tag_list.add('tag-1', 'tag-3')
-    websites.each { |website| website.save }
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-1', 'tag-2')
+    @websites[2].tag_list.add('tag-1', 'tag-3')
+    @websites.each { |website| website.save }
     
     tagged_websites = @tag_matcher.find(Website, ['tag-1', 'tag-2', 'tag-3'])
     
@@ -57,11 +54,10 @@ RSpec.describe TagMatcher do
   end
   
   it 'finds all instances of a specified class whose tags are included in the tags for a tagged instance (of another class)' do
-    websites = create_list(:website, 3)
-    websites[0].tag_list.add('tag-1')
-    websites[1].tag_list.add('tag-2', 'tag-3')
-    websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
-    websites.each { |website| website.save }
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-2', 'tag-3')
+    @websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
+    @websites.each { |website| website.save }
     images = create_list(:image, 6)
     images[0].tag_list.add('tag-1')
     images[1].tag_list.add('tag-2', 'tag-3')
@@ -71,8 +67,19 @@ RSpec.describe TagMatcher do
     images[5].tag_list.add('tag-7', 'tag-8')
     images.each { |image| image.save }
 
-    matched_images = @tag_matcher.match(Image, websites[2].tag_list)
+    matched_images = @tag_matcher.match(Image, @websites[2].tag_list)
     
     expect(matched_images.length).to eq(4)
+  end
+  
+  it 'returns the distinct tag list for a collection of tagged objects' do
+    @websites[0].tag_list.add('tag-1')
+    @websites[1].tag_list.add('tag-2', 'tag-3')
+    @websites[2].tag_list.add('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
+    
+    distinct_tag_list = @tag_matcher.distinct_tag_list(@websites)
+    
+    expect(distinct_tag_list.count).to eq(5)
+    expect(distinct_tag_list).to include('tag-1', 'tag-2', 'tag-3', 'tag-4', 'tag-5')
   end
 end
