f45cf7164f0255d78910e356ac39d66826afbda2
Praveen Angyan <angyan@usc.edu>
2017-02-07 00:23:31 +0000

    Fix publish messages to publish a week in advance

--- app/jobs/publish_messages_job.rb
+++ app/jobs/publish_messages_job.rb
@@ -3,6 +3,7 @@ class PublishMessagesJob < ActiveJob::Base
  
   def perform
     pending_messages = Message.where(publish_status: :pending)
+    pending_messages = pending_messages.where('social_network_publish_date <= ?', Time.now + 7.days)
 
     pending_messages.all.each do |pending_message|
       BufferClient.create_update(pending_message)
--- spec/jobs/publish_messages_job_spec.rb
+++ spec/jobs/publish_messages_job_spec.rb
@@ -4,15 +4,22 @@ RSpec.describe PublishMessagesJob, type: :job do
   include ActiveJob::TestHelper
   
   before do
+    allow(Time).to receive(:now).and_return(Time.new(2010, 1, 1, 0, 0, 0))    
+    allow(BufferClient).to receive(:create_update)
     ActiveJob::Base.queue_adapter = :test
     website = create(:website)
     experiment = create(:experiment)
     @messages = create_list(:message, 10, message_generating: experiment, promotable: website)
-    (5..9).each do |index|
-      @messages[index].publish_status = :published_to_buffer
+    (0..4).each do |index|
+      @messages[index].publish_status = :pending
+      @messages[index].social_network_publish_date = Time.now + 1.day
       @messages[index].save
     end
-    allow(BufferClient).to receive(:create_update)
+    (5..9).each do |index|
+      @messages[index].publish_status = :published_to_buffer
+      @messages[index].social_network_publish_date = Time.now + 30.days
+      @messages[index].save
+    end
   end
   
   it 'queues the job' do
@@ -27,6 +34,27 @@ RSpec.describe PublishMessagesJob, type: :job do
     (0..4).each do |index|
       expect(BufferClient).to receive(:create_update).with(@messages[index])
     end
+
+    perform_enqueued_jobs { PublishMessagesJob.perform_later }
+  end
+
+  it 'executes perform and publishes pending messages that need to be published to social networks in the next 7 days' do
+    @messages[0].social_network_publish_date = Time.new(2010, 1, 1, 0, 0, 0)
+    @messages[1].social_network_publish_date = Time.new(2010, 1, 6, 0, 0, 0)
+    @messages[2].social_network_publish_date = Time.new(2010, 1, 8, 0, 0, 0)
+    @messages[3].social_network_publish_date = Time.new(2010, 1, 9, 0, 0, 0)
+    @messages[4].social_network_publish_date = Time.new(2010, 1, 10, 0, 0, 0)
+    (0..4).each { |index| @messages[index].save }
+
+    # Only publish pending messages to Buffer up to a week in advance (social_network_publish_date <= 7 days from today)
+    (0..2).each do |index|
+      expect(BufferClient).to receive(:create_update).with(@messages[index])
+    end
+    # Ignore pending messages that are scheduled more than a week ahead
+    (3..4).each do |index|
+      expect(BufferClient).not_to receive(:create_update).with(@messages[index])
+    end
+
     perform_enqueued_jobs { PublishMessagesJob.perform_later }
   end
 
