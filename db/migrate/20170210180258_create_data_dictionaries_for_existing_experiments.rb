39b741e162956c5d3119e7ed2ae538a35b7c79e8
Alicia MacLennan <aflyingfemme@gmail.com>
2017-02-10 18:26:16 +0000

    Fix migrations so that existing experiments to have posting_times and data_dictionary

--- db/migrate/20170210180258_create_data_dictionaries_for_existing_experiments.rb
+++ db/migrate/20170210180258_create_data_dictionaries_for_existing_experiments.rb
@@ -4,7 +4,7 @@ class CreateDataDictionariesForExistingExperiments < ActiveRecord::Migration
       DataDictionary.create_data_dictionary(experiment)
     end
   end
-  
+
   def down
     Experiment.all.each do |experiment|
       experiment.data_dictionary.destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209211148) do
+ActiveRecord::Schema.define(version: 20170210180258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,8 +69,6 @@ ActiveRecord::Schema.define(version: 20170209211148) do
     t.text     "hashtags"
   end
 
-<<<<<<< HEAD
-=======
   create_table "data_dictionaries", force: :cascade do |t|
     t.integer  "experiment_id"
     t.datetime "created_at",    null: false
@@ -91,7 +89,6 @@ ActiveRecord::Schema.define(version: 20170209211148) do
     t.text     "value_mapping"
   end
 
->>>>>>> development
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
