71a279952dbe583f6d65aec8e91eddbf6c37bda3
Praveen Angyan <angyan@usc.edu>
2017-02-15 23:06:27 +0000

    Add timeline library

---
+++ lib/timeline.rb
@@ -0,0 +1,22 @@
+class Timeline
+  attr_accessor :events
+  
+  def initialize
+    self.events = []
+  end
+  
+  def add_event(date, summary, description)
+    events << OpenStruct.new(:date => date, :summary => summary, :description => description)
+  end
+  
+  def self.create_default_timeline(experiment)
+    timeline = Timeline.new
+
+    timeline.add_event(experiment.message_distribution_start_date - 3.days, 'Finish experiment setup; Message generation will be locked', 'Trial Promoter will no longer allow messages to be generated. Trial Promoter will send messages to Buffer (upto a week in advance) for distribution to social networks.')
+    timeline.add_event(experiment.message_distribution_start_date, 'Buffer starts distributing messages to social networks.', '')
+    timeline.add_event(experiment.end_date, 'Buffer distributes the last set of messages to social networks.', '')
+    timeline.add_event(experiment.end_date + 3.days, 'Trial Promoter stops collecting data.', '')
+
+    timeline
+  end
+end
---
+++ spec/lib/timeline_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe Timeline do
+  before do
+    @experiment = build(:experiment)
+  end
+  
+  it 'can be initialized' do
+    timeline = Timeline.new
+    
+    expect(timeline.events).to eq([])
+  end
+  
+  it 'adds events correctly' do
+    timeline = Timeline.new
+
+    timeline.add_event(DateTime.new(2000, 1, 1, 12, 30, 0), 'Summary', 'Description')
+    
+    expect(timeline.events.count).to eq(1)
+    expect(timeline.events[0].date).to eq(DateTime.new(2000, 1, 1, 12, 30, 0))
+    expect(timeline.events[0].summary).to eq('Summary')
+    expect(timeline.events[0].description).to eq('Description')
+  end
+  
+  it 'creates a default timeline for an experiment' do
+    timeline = Timeline.create_default_timeline(@experiment)
+    
+    expect(timeline.events.count).to eq(4)
+    expect(timeline.events[0].date).to eq(@experiment.message_distribution_start_date - 3.days)
+    expect(timeline.events[0].summary).to eq('Finish experiment setup; Message generation will be locked')
+    expect(timeline.events[0].description).to eq('Trial Promoter will no longer allow messages to be generated. Trial Promoter will send messages to Buffer (upto a week in advance) for distribution to social networks.')
+    expect(timeline.events[1].date).to eq(@experiment.message_distribution_start_date)
+    expect(timeline.events[1].summary).to eq('Buffer starts distributing messages to social networks.')
+    expect(timeline.events[1].description).to eq('')
+    expect(timeline.events[2].date).to eq(@experiment.end_date)
+    expect(timeline.events[2].summary).to eq('Buffer distributes the last set of messages to social networks.')
+    expect(timeline.events[2].description).to eq('')
+    expect(timeline.events[3].date).to eq(@experiment.end_date + 3.days)
+    expect(timeline.events[3].summary).to eq('Trial Promoter stops collecting data.')
+    expect(timeline.events[3].description).to eq('')
+  end
+end
