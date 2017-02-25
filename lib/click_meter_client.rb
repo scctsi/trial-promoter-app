6c6ae7f71efa58fa03438b4206ed277803050ed4
Praveen Angyan <angyan@usc.edu>
2017-02-25 00:29:31 +0000

    Implement basic ClickMeter API

--- lib/click_meter_client.rb
+++ lib/click_meter_client.rb
@@ -2,20 +2,23 @@ class ClickMeterClient
   include HTTParty
   
   # This class is a facade to the Click Meter API
-  def self.post_request_body_for_create_tracking_link(experiment, message)
+  def self.post_request_body_for_create_tracking_link(group_id, url, title, name)
     post_request_body = {}
     
     # REF: https://support.clickmeter.com/hc/en-us/articles/211036826-How-to-create-a-tracking-link
-    post_request_body[:type] = '0' 
-    post_request_body[:title] = message.to_param
-    post_request_body[:groupId] = '572279'
-    post_request_body[:name] = message.to_param
-    post_request_body[:typeTL] = { domainId: '1501', redirectType: '301', url: TrackingUrl.campaign_url(message) }
+    # title is the name used in the dashboard of ClickMeter for the created link
+    # name is used in the tracking link. e.g.: http://trk.as/name
+    post_request_body["type"] = 0
+    post_request_body["title"] = title
+    post_request_body["groupId"] = group_id
+    post_request_body["name"] = name
+    post_request_body["typeTL"] = { "domainId" => 1501, "redirectType" => 301, "url" => url }
     
-    post_request_body  
+    post_request_body
   end
   
-  def self.create_tracking_link(experiment, message)
-    response = get('http://apiv2.clickmeter.com:80/datapoints', :headers => { 'Content-Type' => 'application/json; charset=UTF-8', 'X-Clickmeter-Authkey' => Setting[:click_meter_api_key]} )
+  def self.create_tracking_link(group_id, url, title, name)
+    p post_request_body_for_create_tracking_link(group_id, url, title, name)
+    response = post('http://apiv2.clickmeter.com:80/datapoints', :body => post_request_body_for_create_tracking_link(group_id, url, title, name).to_json, :headers => { 'Content-Type' => 'application/json; charset=UTF-8', 'X-Clickmeter-Authkey' => Setting[:click_meter_api_key]} )
   end
 end
--- spec/lib/click_meter_client_spec.rb
+++ spec/lib/click_meter_client_spec.rb
@@ -6,31 +6,31 @@ RSpec.describe ClickMeterClient do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:click_meter_api_key).and_return(secrets['click_meter_api_key'])
     allow(ClickMeterClient).to receive(:post).and_call_original
-    @experiment = create(:experiment)
-    @message = create(:message, message_generating: @experiment)
   end
 
   describe "(development only tests)", :development_only_tests => true do
     it 'returns the body of the POST request for creating a tracking link via the ClickMeter API' do
-      post_request_body = ClickMeterClient.post_request_body_for_create_tracking_link(@experiment, @message)
+      post_request_body = ClickMeterClient.post_request_body_for_create_tracking_link(571973, 'http://www.sc-ctsi.org', '1-tcors-message-1', BijectiveFunction.encode(1))
 
-      expect(post_request_body[:type]).to eq('0')
-      expect(post_request_body[:title]).to eq(@message.to_param)
-      expect(post_request_body[:groupId]).to eq('572279')
-      expect(post_request_body[:name]).to eq(@message.to_param)
-      expect(post_request_body[:typeTL]).to eq( { domainId: '1501', redirectType: '301', url: TrackingUrl.campaign_url(@message) })
+      expect(post_request_body["type"]).to eq(0)
+      expect(post_request_body["title"]).to eq('1-tcors-message-1')
+      expect(post_request_body["groupId"]).to eq(571973)
+      expect(post_request_body["name"]).to eq(BijectiveFunction.encode(1))
+      expect(post_request_body["typeTL"]).to eq( { "domainId" => 1501, "redirectType" => 301, "url" => 'http://www.sc-ctsi.org' })
     end
     
     it 'uses the Click Meter API To create a tracking link' do
       url = ''
   
       VCR.use_cassette 'click_meter/create_tracking_link' do
-        url = ClickMeterClient.create_tracking_link(@experiment, @message)
+        url = ClickMeterClient.create_tracking_link(571973, 'http://www.sc-ctsi.org', 'SC CTSI', 'abc')
       end
-      
-      puts url
 
-      expect(ClickMeterClient).to have_received(:post).with('http://apiv2.clickmeter.com/datapoints', :body => ClickMeterClient.post_request_body_for_create_tracking_link(@experiment, @message), :headers => { 'Content-Type' => 'application/json; charset=UTF-8', 'X-Clickmeter-Authkey' => Setting[:click_meter_api_key] })
+      # puts ClickMeterClient.post_request_body_for_create_tracking_link(@experiment, @message).to_json
+      
+      # puts url
+
+      expect(ClickMeterClient).to have_received(:post).with('http://apiv2.clickmeter.com:80/datapoints', :body => ClickMeterClient.post_request_body_for_create_tracking_link(571973, 'http://www.sc-ctsi.org', 'SC CTSI', 'abc').to_json, :headers => { 'Content-Type' => 'application/json; charset=UTF-8', 'X-Clickmeter-Authkey' => Setting[:click_meter_api_key] })
     end
   end
 end
