9eab1f48f617d4812dfb83828b804c83c348b1b1
Praveen Angyan <angyan@usc.edu>
2017-03-03 08:59:37 -0800

    Add filename to uploaded files (#82)
    * Fix test errors
    
    * Add loading indicator to view images
    
    * Store original filenames of uploaded images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
     experiment = Experiment.find(params[:experiment_id])
 
     # Import images
-    image_importer = ImageImporter.new(params[:image_urls], experiment.to_param)
+    image_importer = ImageImporter.new([params[:image_urls], params[:original_filenames]], experiment.to_param)
     image_importer.import
 
     render json: { success: true, imported_count: params[:image_urls].length }
