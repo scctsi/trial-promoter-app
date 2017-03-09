6c1273895cc17f452dd33434d32ad46383eafcdc
Praveen Angyan <angyan@usc.edu>
2017-03-09 01:19:02 +0000

    Fix broken cassette

--- spec/lib/csv_file_reader_spec.rb
+++ spec/lib/csv_file_reader_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe CsvFileReader do
   
   it 'successfully reads a CSV file from a URL' do
     csv_url = 'http://sc-ctsi.org/trial-promoter/message_templates.csv'
-    sample_csv_content = [["content", "platform", "hashtags", "tag_list", "website_url", "website_name"], ["This is the first message template.", "twitter", "#hashtag1, #hashtag2", "theme-1, stem-1", "http://www.url1", "Smoking cessation"], ["This is the second message template.", "twitter", "#hashtag1, #hashtag2", "theme-1, stem-2", "http://www.url1", "Smoking cessation"]]
+    sample_csv_content = [["content", "platforms", "hashtags", "tag_list", "website_url", "website_name"], ["This is the first message template.", "twitter", "#hashtag1, #hashtag2", "theme-1, stem-1", "http://www.url1.com", "Smoking cessation"], ["This is the second message template.", "twitter", "#hashtag1, #hashtag2", "theme-1, stem-2", "http://www.url2.com", "Smoking cessation"]]
     csv_content = ''
     
     VCR.use_cassette 'csv_file_reader/read' do
