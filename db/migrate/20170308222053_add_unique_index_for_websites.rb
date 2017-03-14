b035f0ab6d7337e66a724a0e95856bee0e64df7b
Praveen Angyan <angyan@usc.edu>
2017-03-14 19:56:13 +0000

    Remove migration that works with websites

--- db/migrate/20170308222053_add_unique_index_for_websites.rb
+++
@@ -1,9 +0,0 @@
-class AddUniqueIndexForWebsites < ActiveRecord::Migration
-  def change
-    Website.all.each do |website|
-      website.url = "#{website.id}"
-      website.save
-    end
-    add_index :websites, :url, unique: true
-  end
-end
