01a801a56b82aff9e3f1bf935d8a0a5de1fdbdd2
a mac <aflyingfemme@gmail.com>
2017-04-03 13:53:58 -0700

    Make small changes (#124)
    * Updated gemfile.lock
    
    * preserve changes to app.yml for caching aws tokens
    
    * Add changes to setup and dashboard
    
    * Add cosmetic changes
    
    * Fix test for timeline
    
    * Start revising dashboard
    
    * Add remaining changes
    
    * Finish changes to setup and dashboard
    
    * Update app.css.scss

--- spec/lib/timeline_spec.rb
+++ spec/lib/timeline_spec.rb
@@ -5,32 +5,32 @@ RSpec.describe Timeline do
     @experiment = build(:experiment)
     allow(@experiment).to receive(:end_date).and_return(@experiment.message_distribution_start_date + 10.days)
   end
-  
+
   it 'can be initialized' do
     timeline = Timeline.new
-    
+
     expect(timeline.events).to eq([])
   end
-  
+
   it 'adds events correctly' do
     timeline = Timeline.new
 
     timeline.add_event(DateTime.new(2000, 1, 1, 12, 30, 0), 'Summary', 'Description')
-    
+
     expect(timeline.events.count).to eq(1)
     expect(timeline.events[0].date).to eq(DateTime.new(2000, 1, 1, 12, 30, 0))
     expect(timeline.events[0].summary).to eq('Summary')
     expect(timeline.events[0].description).to eq('Description')
   end
-  
+
   it 'builds a default timeline for an experiment' do
-    
+
     timeline = Timeline.build_default_timeline(@experiment)
-    
+
     expect(timeline.events.count).to eq(4)
     expect(timeline.events[0].date).to eq(@experiment.message_distribution_start_date - 3.days)
     expect(timeline.events[0].summary).to eq('Finish experiment setup; Message generation will be locked')
-    expect(timeline.events[0].description).to eq('Trial Promoter will no longer allow messages to be generated. Trial Promoter will send messages to Buffer (upto a week in advance) for distribution to social networks.')
+    expect(timeline.events[0].description).to eq('Trial Promoter will no longer allow messages to be generated. Trial Promoter will send messages to Buffer (up to a week in advance) for distribution to social networks.')
     expect(timeline.events[1].date).to eq(@experiment.message_distribution_start_date)
     expect(timeline.events[1].summary).to eq('Buffer starts distributing messages to social networks.')
     expect(timeline.events[1].description).to eq('')
