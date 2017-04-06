f7f7d753c8fc5308e5994bd69d352d2a7724a03b
Praveen Angyan <angyan@usc.edu>
2017-04-06 11:08:59 -0700

    Remove colon (#136)
    * Add migration for parsing analytics files
    
    * Add status field for processing
    
    * Change comment
    
    * Add analytics data parser spec
    
    * Fix selection of social media profiles
    
    * Add missing library file
    
    * Start implementing analytics data parser
    
    * Implement two small changes to front end
    
    * Remove colon
    
    * Change image values in experiment setup

--- lib/analytics_data_parser.rb
+++ lib/analytics_data_parser.rb
@@ -1,6 +1,5 @@
 class AnalyticsDataParser
   def self.parse(data)
-    # Step 1: Find the column which has the service update id
     # service_update_id_column_index = @data.columns.index('service_update_id')
     
     # # Step 2: Go through every row in the data
