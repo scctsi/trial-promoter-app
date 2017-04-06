28f5bfae2efc6e9ef62e85339769a09a5b408520
Praveen Angyan <angyan@usc.edu>
2017-04-06 20:24:52 +0000

    Remove diff marker

--- lib/analytics_data_parser.rb
+++ lib/analytics_data_parser.rb
@@ -1,7 +1,6 @@
 class AnalyticsDataParser
   def self.parse(data)
     # Step 1: Find the column which has the service update id
-<<<<<<< HEAD
     # service_update_id_column_index = @data.columns.index('service_update_id')
     
     # # Step 2: Go through every row in the data
@@ -9,14 +8,5 @@ class AnalyticsDataParser
     # data.rows.each do |row|
     #   BufferUpdate.where(service_update_id: row[service_update_id_column_index])
     # end
-=======
-    service_update_id_column_index = @data.columns.index('service_update_id')
-    
-    # Step 2: Go through every row in the data
-    metrics = {}
-    data.rows.each do |row|
-      BufferUpdate.where(service_update_id: row[service_update_id_column_index])
-    end
->>>>>>> remove-colon
   end
 end
