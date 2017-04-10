d19f6fc27996075b7f61872d87bb72d05067ecf1
Praveen Angyan <angyan@usc.edu>
2017-04-10 16:47:39 +0000

    Add processing for twitter files

--- lib/analytics_data_parser.rb
+++ lib/analytics_data_parser.rb
@@ -1,4 +1,13 @@
 class AnalyticsDataParser
+  def self.convert_to_parseable_data(csv_content, platform, medium)
+    data = OpenStruct.new
+    
+    data.column_headers = ['service_update_id', '', '', '', 'impressions', '', '', 'retweets', 'replies', 'likes', '', 'clicks', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']
+    data.rows = csv_content[1..-1]
+    
+    data
+  end
+  
   def self.parse(data)
     # Step 1: Find the column which has the service update id
     service_update_id_column_index = data.column_headers.index('service_update_id')
@@ -18,5 +27,13 @@ class AnalyticsDataParser
     end
     
     parsed_data
+  end
+  
+  def self.store(parsed_data, source)
+    parsed_data.each do |message_param, metrics|
+      message = Message.find_by_param(message_param)
+      message.metrics << Metric.new(source: source, data: metrics)
+      message.save
+    end
   end
 end
