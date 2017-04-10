6d6d886f17d04f77e7d92a05fc08b9f537e883b6
Praveen Angyan <angyan@usc.edu>
2017-04-10 16:47:39 +0000

    Add processing for twitter files

--- spec/lib/analytics_data_parser_spec.rb
+++ spec/lib/analytics_data_parser_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnalyticsDataParser do
   before do
-    @messages = create_list(:message, 3)
+    @messages = create_list(:message, 3, platform: :twitter)
     @messages[0..1].each do |message|
       message.buffer_update = build(:buffer_update, :service_update_id => "service_update_id_#{message.id}")
       message.buffer_update.save
@@ -18,5 +18,29 @@ RSpec.describe AnalyticsDataParser do
     parsed_data = AnalyticsDataParser.parse(@data)
 
     expect(parsed_data).to eq({ @messages[0].to_param => { 'impressions' => 1, 'likes' => 2, 'shares' => 3, 'clicks' => 7}, @messages[1].to_param => { 'ga:sessions' => 2, 'ga:users' => 3}, @messages[1].to_param => { 'impressions' => 8, 'likes' => 9, 'shares' => 10, 'clicks' => 14} })
+  end
+
+  it 'stores parsed data on the respective messages (matched by service_update_id)' do
+    parsed_data = { @messages[0].to_param => { 'impressions' => 1, 'likes' => 2, 'shares' => 3, 'clicks' => 7}, @messages[1].to_param => { 'ga:sessions' => 2, 'ga:users' => 3}, @messages[1].to_param => { 'impressions' => 8, 'likes' => 9, 'shares' => 10, 'clicks' => 14} }
+
+    AnalyticsDataParser.store(parsed_data, :twitter)
+    
+    @messages[0].reload
+    expect(@messages[0].metrics.count).to eq(1)
+    expect(@messages[0].metrics[0].data).to eq({ 'impressions' => 1, 'likes' => 2, 'shares' => 3, 'clicks' => 7})
+    expect(@messages[0].metrics[0].source).to eq(:twitter)
+    @messages[1].reload
+    expect(@messages[1].metrics.count).to eq(1)
+    expect(@messages[1].metrics[0].data).to eq({ 'impressions' => 8, 'likes' => 9, 'shares' => 10, 'clicks' => 14})
+    expect(@messages[1].metrics[0].source).to eq(:twitter)
+  end
+  
+  it 'converts the contents of a CSV file exported from analytics.twitter.com (organic Twitter data) to parseable data' do
+    csv_content = CSV.read("#{Rails.root}/spec/fixtures/tweet_activity_metrics_TCORSStgOrg_20170401_20170408_en.csv")
+
+    parseable_data = AnalyticsDataParser.convert_to_parseable_data(csv_content, :twitter, :organic)
+
+    expect(parseable_data.column_headers).to eq(['service_update_id', '', '', '', 'impressions', '', '', 'retweets', 'replies', 'likes', '', 'clicks', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''])
+    expect(parseable_data.rows).to eq(csv_content[1..-1])
   end
 end
--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -17,20 +17,5 @@ RSpec.describe Metric, type: :model do
   it { is_expected.to validate_presence_of :source }
   it { is_expected.to enumerize(:source).in(:buffer, :twitter, :facebook, :instagram, :google_analytics) }
   it { is_expected.to belong_to(:message) }
-  
-  it 'stores data as a hash' do
-    data = {
-      "reach": 2460,
-      "clicks": 56,
-      "retweets": 20,
-      "favorites": 1,
-      "mentions": 1
-    }
-    metric = Metric.new(source: :twitter, data: data)
-
-    metric.save
-    metric.reload
-    
-    expect(metric.data).to eq(data)
-  end
+  it { is_expected.to serialize(:data).as(Hash) }
 end
