b89f3f841427bedb0834f41ceccb262346f35a87
Praveen Angyan <angyan@usc.edu>
2017-04-12 13:37:43 -0700

    Process analytics files (#158)
    * Add test for analytics files
    
    * Add ability to process all analytics files

--- spec/policies/analytics_file_policy_spec.rb
+++ spec/policies/analytics_file_policy_spec.rb
@@ -9,23 +9,27 @@ RSpec.describe AnalyticsFilePolicy, type: :policy do
     let(:user) { create(:user) }
 
     it { should_not be_permitted_to(:update) }
+    it { should_not be_permitted_to(:process_all_files) }
   end
 
   context "for a administrator" do
     let(:user) { create(:administrator) }
 
     it { should be_permitted_to(:update) }
+    it { should be_permitted_to(:process_all_files) }
   end
 
   context "for a statistician" do
     let(:user) { create(:statistician) }
 
     it { should_not be_permitted_to(:update) }
+    it { should_not be_permitted_to(:process_all_files) }
   end
 
   context "for a read_only" do
     let(:user) { create(:read_only) }
 
     it { should_not be_permitted_to(:update) }
+    it { should_not be_permitted_to(:process_all_files) }
   end
 end
