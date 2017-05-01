b4b1709b9678cdfd88da080aa5fbb6821ce71bfd
Alicia MacLennan <aflyingfemme@gmail.com>
2017-05-01 15:44:26 -0700

    Add times to factory clicks; tests pass

--- spec/factories/click.rb
+++ spec/factories/click.rb
@@ -3,15 +3,15 @@ FactoryGirl.define do
     click_meter_tracking_link
 
     trait :wednesday_clicks do
-      click_time "26 April 2017"
+      click_time "26 April 2017 19:21"
     end
 
     trait :tuesday_clicks do
-      click_time "25 April 2017"
+      click_time "25 April 2017 17:38"
     end
 
     trait :monday_clicks do
-      click_time "24 April 2017"
+      click_time "24 April 2017 00:01"
     end
 
     trait :sunday_clicks do
