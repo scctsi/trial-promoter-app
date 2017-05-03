fdeaa2655dbd430befff3891f66da04b0dff87cd
Alicia MacLennan <aflyingfemme@gmail.com>
2017-05-03 15:53:45 -0700

    Add job for click meter api; tests pass

--- spec/jobs/get_analytics_from_click_meter_job_spec.rb
+++ spec/jobs/get_analytics_from_click_meter_job_spec.rb
@@ -5,30 +5,27 @@ RSpec.describe GetAnalyticsFromClickMeterJob, type: :job do
   
   before do
     ActiveJob::Base.queue_adapter = :test
-    @click_meter_tracking_links = Message.all.map{ |message| message.click_meter_tracking_link }
-    @click_meter_tracking_link.each { |link| link.clicks << create_list(:click, 3, :sunday_clicks) }
+    @message = create(:message)
+    @message.click_meter_tracking_link = create(:click_meter_tracking_link)
+    @message.click_meter_tracking_link.clicks << create_list(:click, 3, :sunday_clicks) 
+    @message.save
   end
   
   it 'queues the job' do
-    expect { GetAnalyticsFromClickMeterJob.perform_later(@click_meter_tracking_links) }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+    expect { GetAnalyticsFromClickMeterJob.perform_later(@experiment) }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
   end
   
   it 'queues the job in the default queue' do
     expect(GetAnalyticsFromClickMeterJob.new.queue_name).to eq('default')
+  end 
+
+  it 'executes perform' do
+    # @click_meter_tracking_links.each { |tracking_link|
+    # p @message.click_meter_tracking_link.click_meter_id 
+      expect(ClickMeterClient).to receive(:get_clicks).with(@message.click_meter_tracking_link)
+    # }
+     perform_enqueued_jobs { GetAnalyticsFromClickMeterJob.perform_later(@message.click_meter_tracking_link) }
   end
-
-  it 'get clicks for tracking link' do
-    @click_meter_tracking_links.each { |tracking_link|
-      expect(ClickMeterClient).to receive(:get_clicks).with(tracking_link.clicks)
-    }
-    perform_enqueued_jobs { GetAnalyticsFromClickMeterJob.perform_later }
-  end
-
-
-  xit 'get clicks for tracking link' do
-    expect(ClickMeterClient).to receive(:get_clicks).with(tracking_link.clicks)
-  end
-
 
   after do
     clear_enqueued_jobs
