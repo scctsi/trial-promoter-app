33006c71a79a67fa52c42345fffd94ce811c895f
Alicia MacLennan <aflyingfemme@gmail.com>
2017-05-09 02:32:50 -0700

    Fix typo

--- app/jobs/get_analytics_from_click_meter_job.rb
+++ app/jobs/get_analytics_from_click_meter_job.rb
@@ -4,8 +4,8 @@ class GetAnalyticsFromClickMeterJob < ActiveJob::Base
   def perform
     published_messages = Message.where(publish_status: :published_to_social_network)
     published_messages.each do |message|
-    ClickMeterClient.get_clicks(tracking_link)
-    # Throttler.throttle(10)
-
+      ClickMeterClient.get_clicks(tracking_link)
+    # Throttler.throttle(10)  
+    end
   end
 end
