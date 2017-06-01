d4840ad4634b07376e4febdfd8cc84084e4983d5
Praveen Angyan <angyan@usc.edu>
2017-06-01 23:30:41 +0000

    Update buffer update if Buffer indicates sent

--- lib/buffer_client.rb
+++ lib/buffer_client.rb
@@ -32,17 +32,19 @@ class BufferClient
 
   def self.get_update(message)
     response = get("https://api.bufferapp.com/1/updates/#{message.buffer_update.buffer_id}.json?access_token=#{Setting[:buffer_access_token]}")
-    message.buffer_update.status = response.parsed_response["status"]
-    sent_time = Time.at(response.parsed_response["sent_at"])
-    message.buffer_update.sent_from_date_time = sent_time if message.buffer_update.status == 'sent'
-    message.buffer_update.service_update_id = response.parsed_response["service_update_id"]
-    # It's usually a bad idea to do what we do in the next line, namely copy the service_update_id to the message (the service_update_id is no longer DRY, it is repeated in two models).
-    # However it makes sense in this case, because 1) it's convenient to access the social_network_id from the message and 2) the social_network_id should remain unchanged for ever.
-    message.social_network_id = message.buffer_update.service_update_id
-    message.metrics << Metric.new(source: :buffer, data: response.parsed_response["statistics"])
-    message.publish_status = :published_to_social_network
-    message.buffer_update.save
-    message.save
+    if response.parsed_response["status"] == 'sent'
+      message.buffer_update.status = :sent
+      sent_time = Time.at(response.parsed_response["sent_at"])
+      message.buffer_update.sent_from_date_time = sent_time if message.buffer_update.status == 'sent'
+      message.buffer_update.service_update_id = response.parsed_response["service_update_id"]
+      # It's usually a bad idea to do what we do in the next line, namely copy the service_update_id to the message (the service_update_id is no longer DRY, it is repeated in two models).
+      # However it makes sense in this case, because 1) it's convenient to access the social_network_id from the message and 2) the social_network_id should remain unchanged for ever.
+      message.social_network_id = message.buffer_update.service_update_id
+      message.metrics << Metric.new(source: :buffer, data: response.parsed_response["statistics"])
+      message.publish_status = :published_to_social_network
+      message.buffer_update.save
+      message.save
+    end
   end
 
   def self.create_update(message)
