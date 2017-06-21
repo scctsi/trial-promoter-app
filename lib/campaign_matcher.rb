27a35a270865a371da51683673d2379fe9b24304
Alicia MacLennan <aflyingfemme@gmail.com>
2017-06-21 19:35:17 +0000

    Delete date-checking, duplicate capaign names

--- lib/campaign_matcher.rb
+++ lib/campaign_matcher.rb
@@ -1,18 +1,26 @@
 class CampaignMatcher
 
   def self.match_campaign_id(messages, campaigns)
+    campaign_name_repeats = campaigns.group_by{|campaign| campaign.name}.select{|key,value| key if value.count > 1 }
+    campaign_name_rejects = []
+    campaign_name_repeats.each do |key, value|
+      campaign_name_rejects << campaigns.select{|campaign| campaign.name.include?(key)}
+      campaigns.reject!{|campaign| campaign.name.include?(key)}
+    end
     campaigns.each do |campaign|
       campaign.name = clean_ad_name(campaign.name)
 
       messages.each do |message|
-        next if !message.matchable? || !date_match?(message, campaign.start_date) || instagram?(message)
+        next if !message.matchable? || instagram?(message)
         message.campaign_id = campaign.id if message.content.include?(campaign.name)
       end
     end
+
     messages.each do |message|
       set_unmatchable(message)
       message.save
     end
+    return campaign_name_rejects
   end
 end
 
@@ -20,10 +28,6 @@ def clean_ad_name(campaign_name)
   campaign_name.gsub!("...", "")
   campaign_name.gsub!("Post: ", "")
   campaign_name.gsub!('"', "")
-end
-
-def date_match?(message, campaign_start_date)
-  message.scheduled_date_time == Date.parse(campaign_start_date).to_s
 end
 
 def set_unmatchable(message)
--- spec/lib/campaign_matcher_spec.rb
+++ spec/lib/campaign_matcher_spec.rb
@@ -12,31 +12,43 @@ RSpec.describe CampaignMatcher do
 
     @messages.each{ |message| message.publish_status = 'published_to_social_network' }
 
-    @messages[0].scheduled_date_time = '5/9/2016'
-    @messages[1].scheduled_date_time = '5/11/2016'
-    @messages[2].scheduled_date_time = '5/10/2016'
-
     @messages.each{ |message| message.save }
 
     @campaigns = []
-    (0..2).each { |index| @campaigns[index] = OpenStruct.new }
+    (0..6).each { |index| @campaigns[index] = OpenStruct.new }
     @campaigns[0].name = 'Post: "Nicotine is highly addictive. Every day in the US..."'
     @campaigns[0].id = '6074485202639'
     @campaigns[0].start_date ="5/9/2016"
 
     @campaigns[1].name = 'Post: "Every day in the US 700+ youth become daily smokers. Nicotine is highly addictive..."'
     @campaigns[1].id = '3074485264939'
-    @campaigns[1].start_date ="5/10/2016"
+    @campaigns[1].start_date ="5/9/2016"
 
     @campaigns[2].name = 'Post: "Every day in the US 700+ youth become daily smokers..."'
-    @campaigns[2].id = '3075555264939'
-    @campaigns[2].start_date ="5/12/2016"
+    @campaigns[2].id = '3075555264933'
+    @campaigns[2].start_date ="5/9/2016"
+
+    @campaigns[3].name = 'Post: "Every day in the US 700+ youth become daily smokers..."'
+    @campaigns[3].id = '4075555364933'
+    @campaigns[3].start_date ="5/9/2016"
+
+    @campaigns[4].name = 'Post: "Every day in the US 700+ youth become daily smokers..."'
+    @campaigns[4].id = '5075555464935'
+    @campaigns[4].start_date ="5/9/2016"
+
+    @campaigns[5].name = 'Post: "Every day in the US 800+ youth become daily smokers..."'
+    @campaigns[5].id = '4075555364933'
+    @campaigns[5].start_date ="5/9/2016"
+
+    @campaigns[6].name = 'Post: "Every day in the US 800+ youth become daily smokers..."'
+    @campaigns[6].id = '5075555464935'
+    @campaigns[6].start_date ="5/9/2016"
 
   end
 
   describe '#match_campaign_id' do
-    it 'assigns the matching campaign id to the original message using the ad name (truncated post message)' do
-      CampaignMatcher.match_campaign_id(@messages, @campaigns)
+    it 'assigns the matching campaign id to the original message using the ad name (truncated post message) and returns duplicate messages to be assigned manually' do
+      rejects = CampaignMatcher.match_campaign_id(@messages, @campaigns)
 
       @messages.each{ |message| message.reload }
 
@@ -44,21 +56,14 @@ RSpec.describe CampaignMatcher do
       expect(@messages[1].campaign_id).to eq(nil)
       expect(@messages[2].campaign_id).to eq(@campaigns[1].id)
       expect(@messages[3].campaign_id).to eq(nil)
+      expect(rejects.count).to eq(2)
+      expect(rejects[0][0].name).to eq('Post: "Every day in the US 700+ youth become daily smokers..."')
     end
   end
 
   describe '#clean_ad_name' do
     it 'removes the extra characters from the campaign name analytics file' do
       expect(clean_ad_name(@campaigns[0].name)).to eq("Nicotine is highly addictive. Every day in the US")
-    end
-  end
-
-  describe '#date_match?' do
-    it 'checks that the message scheduled_date_time matches the campaign' do
-      expect(date_match?(@messages[0], @campaigns[0].start_date)).to eq(true)
-      expect(date_match?(@messages[1], @campaigns[1].start_date)).to eq(false)
-      expect(date_match?(@messages[2], @campaigns[2].start_date)).to eq(false)
-      expect(date_match?(@messages[3], @campaigns[2].start_date)).to eq(false)
     end
   end
 
