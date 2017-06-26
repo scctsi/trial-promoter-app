78ac28020ac1504c8ac81025c4668001a64c2c73
a mac <aflyingfemme@gmail.com>
2017-06-26 11:52:35 -0700

    Add google analytics job (#214)
    * Add google analytics metrics collection and tests
    
    * Delete unnecessary secrets
    
    * Fixed
    
    * Delete file
    
    * Delete file

---
+++ spec/jobs/get_analytics_from_google_analytics_job_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe GetAnalyticsFromGoogleAnalyticsJob, type: :job do
+  include ActiveJob::TestHelper
+
+  before do
+    ActiveJob::Base.queue_adapter = :test
+    @google_analytics_client = double('google_analytics')
+    allow(GoogleAnalyticsClient).to receive(:new).and_return(@google_analytics_client)
+    @data = []
+    allow(@google_analytics_client).to receive(:get_data).and_return(@data)
+    allow(GoogleAnalyticsDataParser).to receive(:parse)
+    allow(GoogleAnalyticsDataParser).to receive(:store)
+  end
+
+  it 'queues the job' do
+    expect { GetAnalyticsFromGoogleAnalyticsJob.perform_later }.to change(ActiveJob::Base.queue_adapter.enqueued_jobs, :size).by(1)
+  end
+
+  it 'queues the job in the default queue' do
+    expect(GetAnalyticsFromGoogleAnalyticsJob.new.queue_name).to eq('default')
+  end
+
+  it 'executes perform' do
+    perform_enqueued_jobs { GetAnalyticsFromGoogleAnalyticsJob.perform_later }
+
+    expect(@google_analytics_client).to have_received(:get_data).with(Date.new(2017,4,19).to_s, DateTime.now.to_date)
+    expect(GoogleAnalyticsDataParser).to have_received(:parse).with(@data)
+    expect(GoogleAnalyticsDataParser).to have_received(:store).with(@data)
+  end
+
+  after do
+    clear_enqueued_jobs
+    clear_performed_jobs
+  end
+end
