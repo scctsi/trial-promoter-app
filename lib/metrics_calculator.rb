4be9212c869ec8048901909ce975727f67981b41
Alicia MacLennan <aflyingfemme@gmail.com>
2017-06-26 18:43:52 +0000

    Delete file

--- lib/metrics_calculator.rb
+++
@@ -1,9 +0,0 @@
-class MetricsCalculator
-  def self.click_rate(clicks, impressions)
-    clicks.to_f/impressions.to_f
-  end
-
-  def self.goal_rate(conversions, sessions)
-    conversions.to_f/sessions.to_f
-  end
-end
