8b47f4eb470d221228ead5b1858e9ce313337f74
Alicia MacLennan <aflyingfemme@gmail.com>
2017-07-17 21:45:07 +0000

    Fix tests with VCR

--- spec/lib/comment_aggregator_spec.rb
+++ spec/lib/comment_aggregator_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CommentAggregator do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:facebook_access_token).and_return(secrets['facebook_access_token'])
     @comment_aggregator = CommentAggregator.new
-    @message = create(:message)
+    @message = build(:message)
     @message.social_network_id = "980601328736431_1056074954522401"
 
     VCR.use_cassette 'comment_aggregator/test_setup' do
@@ -15,7 +15,7 @@ RSpec.describe CommentAggregator do
     end
   end
 
-  describe "development only tests" do
+  describe "(development only tests)", :development_only_tests => true do
     it 'gets the page B Free Of Tobacco' do
 
       expect(@page).not_to be_nil
@@ -23,24 +23,23 @@ RSpec.describe CommentAggregator do
     end
 
     it 'gets the comments associated with a given message' do
-      expect(@aggregated_comments.count).to eq(21)
+      expect(@message.comments.count).to eq(22)
     end
 
     it 'saves the comments associated with a given message' do
-      p @aggregated_comments
-      expect(@aggregated_comments[0].comment_text).to eq("And our federal government puts it in there..")
+      @message.reload
+
+      expect(@message.comments[0].comment_text).to eq( "Cyanide is in apple seeds to lol")
     end
 
     it 'only saves comments once' do
-      comments = @aggregated_comments
-
       VCR.use_cassette 'comment_aggregator/get_comments_once' do
         @comment_aggregator.get_comments(@message.social_network_id, @message)
         @comment_aggregator.get_comments(@message.social_network_id, @message)
 
         @message.reload
 
-        expect(@message.comments.count).to eq(21)
+        expect(@message.comments.count).to eq(22)
       end
     end
   end
