c213c6b9c641a7550efaded658abedc9d87f9f66
a mac <aflyingfemme@gmail.com>
2017-08-15 16:17:46 -0700

    Create google analytics task (#248)
    * Add google analytics task
    
    * Add to_s method to fix error with date in ga job
    
    * Fix test
    
    * Fix data being stored in google analytics job

--- spec/jobs/get_analytics_from_google_analytics_job_spec.rb
+++ spec/jobs/get_analytics_from_google_analytics_job_spec.rb
@@ -8,8 +8,9 @@ RSpec.describe GetAnalyticsFromGoogleAnalyticsJob, type: :job do
     @google_analytics_client = double('google_analytics')
     allow(GoogleAnalyticsClient).to receive(:new).and_return(@google_analytics_client)
     @data = []
+    @parsed_data = []
     allow(@google_analytics_client).to receive(:get_data).and_return(@data)
-    allow(GoogleAnalyticsDataParser).to receive(:parse)
+    allow(GoogleAnalyticsDataParser).to receive(:parse).and_return(@parsed_data)
     allow(GoogleAnalyticsDataParser).to receive(:store)
   end
 
@@ -26,7 +27,7 @@ RSpec.describe GetAnalyticsFromGoogleAnalyticsJob, type: :job do
 
     expect(@google_analytics_client).to have_received(:get_data).with(Date.new(2017,4,19).to_s, DateTime.now.to_date.to_s)
     expect(GoogleAnalyticsDataParser).to have_received(:parse).with(@data)
-    expect(GoogleAnalyticsDataParser).to have_received(:store).with(@data)
+    expect(GoogleAnalyticsDataParser).to have_received(:store).with(@parsed_data)
   end
 
   after do
