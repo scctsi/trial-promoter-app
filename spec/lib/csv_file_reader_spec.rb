b9e401dae972602848c433c05988c69f447bec42
Praveen Angyan <angyan@usc.edu>
2017-08-21 18:36:49 +0000

    Add change to CSV file reader spec

--- spec/lib/csv_file_reader_spec.rb
+++ spec/lib/csv_file_reader_spec.rb
@@ -23,16 +23,18 @@ RSpec.describe CsvFileReader do
 
     expect(csv_content.count).to eq(267)
   end
-  
-  it 'successfully reads a CSV file from a private Dropbox file path' do
-    dropbox_file_path = '/tcors/analytics_files/04-19-2017/tweet_activity_metrics_BeFreeOfTobacco_20170419_20170421_en.csv'
-    parsed_csv_content = ''
-    
-    VCR.use_cassette 'csv_file_reader/read_from_dropbox' do
-      parsed_csv_content = CsvFileReader.read_from_dropbox(dropbox_file_path)
-    end
 
-    expect(parsed_csv_content.size).to eq(4)
-    expect(parsed_csv_content[1]).to eq(["854847321003175936", "https://twitter.com/BeFreeOfTobacco/status/854847321003175936", "#Tobacco use causes 1300 US deaths daily-more than AIDS, alcohol, car accidents, homicides &amp; illegal drugs combined https://t.co/hhkUqOfcCS https://t.co/ZG35eVTqLy", "2017-04-20 00:00 +0000", "51.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0", "0", "0", "0", "0", "0", "0", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"])
+  describe "(development only tests)", :development_only_tests => true do
+    it 'successfully reads a CSV file from a private Dropbox file path' do
+      dropbox_file_path = '/tcors/analytics_files/04-19-2017/tweet_activity_metrics_BeFreeOfTobacco_20170419_20170421_en.csv'
+      parsed_csv_content = ''
+      
+      VCR.use_cassette 'csv_file_reader/read_from_dropbox' do
+        parsed_csv_content = CsvFileReader.read_from_dropbox(dropbox_file_path)
+      end
+  
+      expect(parsed_csv_content.size).to eq(4)
+      expect(parsed_csv_content[1]).to eq(["854847321003175936", "https://twitter.com/BeFreeOfTobacco/status/854847321003175936", "#Tobacco use causes 1300 US deaths daily-more than AIDS, alcohol, car accidents, homicides &amp; illegal drugs combined https://t.co/hhkUqOfcCS https://t.co/ZG35eVTqLy", "2017-04-20 00:00 +0000", "51.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0", "0", "0", "0", "0", "0", "0", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"])
+    end
   end
 end
