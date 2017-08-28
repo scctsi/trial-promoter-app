d8c8ab89f6a95a6b9e57aa8e041af8f38ec7e772
Praveen Angyan <angyan@usc.edu>
2017-08-28 22:58:16 +0000

    Ignore missing message while updating impressions

--- lib/metrics_manager.rb
+++ lib/metrics_manager.rb
@@ -14,18 +14,18 @@ class MetricsManager
       else
         message = Message.find_by_alternative_identifier(key)
       end
-      if !(message.nil?)
-        message.metrics << Metric.new(source: source, data: value)
-        message.save
-      end
+      message.metrics << Metric.new(source: source, data: value)
+      message.save
     end
   end
   
   def self.update_impressions_by_day(date, data)
     data.each do |key, value|
       message = Message.find_by_alternative_identifier(key)
-      message.impressions_by_day[date] = value
-      message.save
+      if !(message.nil?)
+        message.impressions_by_day[date] = value
+        message.save
+      end
     end
   end
 end
