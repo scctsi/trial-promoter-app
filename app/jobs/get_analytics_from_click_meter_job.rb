5999be2d5bbbbae7ffae9d80b531d365b56baa3e
Praveen Angyan <angyan@usc.edu>
2017-08-28 15:04:30 -0700

    Change publish status on click meter job (#309)
    * Change publish status
    
    * Parse times from Clickmeter in Pacific time zone

--- app/jobs/get_analytics_from_click_meter_job.rb
+++ app/jobs/get_analytics_from_click_meter_job.rb
@@ -2,7 +2,7 @@ class GetAnalyticsFromClickMeterJob < ActiveJob::Base
   queue_as :default
 
   def perform
-    updated_messages = Message.where(publish_status: :published_to_social_network)
+    updated_messages = Message.where(publish_status: :published_to_buffer)
     updated_messages.all.each do |message|
       ClickMeterClient.get_clicks(message.click_meter_tracking_link)
       Throttler.throttle(10)
