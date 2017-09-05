8a7702c58cd7ec16612df10d2a8bf13a6b9ae537
Praveen Angyan <angyan@usc.edu>
2017-09-05 11:10:29 -0700

    Add note to messages (#343)
    * Remove click meter columns
    
    * Fix spacing issue in Date
    
    * Add debug code to DailyMetricParser
    
    * Add sleep when parsing impressions
    
    * Fix bug with storing impressions
    
    * Rename notes to note

--- spec/lib/tcors_data_report_mapper_spec.rb
+++ spec/lib/tcors_data_report_mapper_spec.rb
@@ -27,18 +27,6 @@ RSpec.describe TcorsDataReportMapper do
     expect(TcorsDataReportMapper.database_id(@message)).to eq(@message.id)
   end
   
-  it 'maps the click_meter_id to click_meter_id' do
-    @message.click_meter_tracking_link.click_meter_id = 'click_meter_id'
-
-    expect(TcorsDataReportMapper.click_meter_id(@message)).to eq(@message.click_meter_tracking_link.click_meter_id)
-  end
-
-  it 'maps the click_meter_uri to click_meter_uri' do
-    @message.click_meter_tracking_link.click_meter_uri = 'http://www.click_meter.com/uri'
-
-    expect(TcorsDataReportMapper.click_meter_uri(@message)).to eq(@message.click_meter_tracking_link.click_meter_uri)
-  end
-  
   describe 'experiment variables mapping methods' do
     it 'maps the message stem_id to stem' do 
       @message.message_template.experiment_variables['stem_id'] = 'FE51'
