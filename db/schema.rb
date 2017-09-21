ea2a3eeed7e2929617a88075add775328703c07e
Alicia M <aflyingfemme@gmail.com>
2017-09-21 21:07:06 +0000

    Alter schema

--- db/schema.rb
+++ db/schema.rb
@@ -20,19 +20,15 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.integer  "visit_id"
     t.integer  "user_id"
     t.string   "name"
-    t.jsonb    "properties"
     t.datetime "time"
+    t.jsonb    "properties"
   end
 
   add_index "ahoy_events", ["name", "time"], name: "index_ahoy_events_on_name_and_time", using: :btree
   add_index "ahoy_events", ["user_id", "name"], name: "index_ahoy_events_on_user_id_and_name", using: :btree
   add_index "ahoy_events", ["visit_id", "name"], name: "index_ahoy_events_on_visit_id_and_name", using: :btree
 
-  create_table "amplitude_conversion_trackers", force: :cascade do |t|
-    t.string   "user"
-    t.integer  "visit"
-    t.integer  "event"
-    t.string   "message"
+  create_table "analytics_file_sets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -47,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.integer  "message_generating_id"
     t.string   "message_generating_type"
     t.string   "processing_status"
+    t.integer  "analytics_file_set_id"
   end
 
   create_table "buffer_updates", force: :cascade do |t|
@@ -113,6 +110,7 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "message_id"
+  end
 
   create_table "daily_metric_parser_results", force: :cascade do |t|
     t.date     "file_date"
@@ -164,7 +162,6 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.datetime "message_distribution_start_date"
     t.datetime "created_at",                                   null: false
     t.datetime "updated_at",                                   null: false
-    t.boolean  "analytics_file_todos_created"
     t.text     "twitter_posting_times"
     t.text     "facebook_posting_times"
     t.text     "instagram_posting_times"
@@ -236,8 +233,8 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.integer  "message_template_id"
     t.text     "content"
     t.string   "tracking_url",                 limit: 2000
-    t.datetime "created_at",                                                null: false
-    t.datetime "updated_at",                                                null: false
+    t.datetime "created_at",                                null: false
+    t.datetime "updated_at",                                null: false
     t.integer  "website_id"
     t.integer  "message_generating_id"
     t.string   "message_generating_type"
@@ -255,7 +252,6 @@ ActiveRecord::Schema.define(version: 20170912225731) do
     t.string   "campaign_id"
     t.boolean  "backdated"
     t.datetime "original_scheduled_date_time"
-    t.boolean  "campaign_unmatchable",                      default: false
     t.float    "click_rate"
     t.float    "website_goal_rate"
     t.integer  "website_goal_count"
