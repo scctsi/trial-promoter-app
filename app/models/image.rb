58ec3affcdb10c2d5c6dca4c656ec28d946e70dc
Praveen Angyan <angyan@usc.edu>
2017-09-22 15:16:13 -0700

    Add duplicates to images (#389)
    * Add schema for image coding
    
    * Add form/button image codes
    
    * Add self referencing relationship to images for duplicates
    
    * Add image codes interface
    
    * Add tests for controller for image coding

--- app/models/image.rb
+++ app/models/image.rb
@@ -25,9 +25,26 @@ class Image < ActiveRecord::Base
   scope :belonging_to, ->(experiment) { tagged_with(experiment.to_param, on: :experiments) }
 
   has_many :messages
+  has_many :duplicates, class_name: 'Image', foreign_key: 'duplicated_image_id'
+  belongs_to :duplicated_image, class_name: 'Image'
 
   def delete_image_from_s3
     s3 = S3Client.new
     s3.delete(s3.bucket(self.url), s3.key(self.url))
   end
+   
+  def map_codes(code_object) 
+    if code_object == []
+      self.codes = {}
+    else
+      hash = {}
+      code_object.each do |code_pair|
+        key_value = code_pair.split(':') 
+        hash[key_value[0]] = key_value[1] 
+      end
+      self.codes = hash
+    end
+    save
+  end 
 end
+   
