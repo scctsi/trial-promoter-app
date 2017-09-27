ec357f164a0ab1061da9cc60c35c4ecba38d30d7
Alicia M <aflyingfemme@gmail.com>
2017-09-27 20:01:54 +0000

    move code mapping into its own module

---
+++ lib/code_mapper.rb
@@ -0,0 +1,15 @@
+module CodeMapper
+  def map_codes(code_object) 
+    if code_object == []
+      self.codes = {}
+    else
+      hash = {}
+      code_object.each do |code_pair|
+        key_value = code_pair.split(':') 
+        hash[key_value[0]] = key_value[1] 
+      end
+      self.codes = hash
+    end
+    save
+  end
+end
