64a00fe1829e6f7b4ccabeb2da2fa9c3803bf526
Alicia M <aflyingfemme@gmail.com>
2017-10-03 17:42:13 +0000

    Add logger debug

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -28,16 +28,23 @@ class Comment < ActiveRecord::Base
     comment_index = comments_spreadsheet[0].index("Comment")
     comment_date_index = comments_spreadsheet[0].index("Date of Comment")
     comment_username_index = comments_spreadsheet[0].index("Commentator Username")
+    orphan_comments = []
     messages.each do |message|  
       comments_spreadsheet.each do |comments_row| 
         #some comments have a newline character that needs to be removed
         clean_comment = comments_row[message_index].chomp
+        orphan_comments << clean_comment
         if !message.buffer_update.nil? && message.buffer_update.published_text == clean_comment 
           message.comments << Comment.create(comment_text: comments_row[comment_index], comment_date: comments_row[comment_date_index], commentator_username: comments_row[comment_username_index])
+          orphan_comments -= [clean_comment]
         end 
       end
       message.save
     end
+    orphan_comments.each do |comment| 
+      logger.debug { "The comment: " + comment + " could not find a matching message." }
+    end
+    messages.each{|m|  p m.comments }
   end
   
   def save_toxicity_score
