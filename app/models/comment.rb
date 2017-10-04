d78d17ba3efb59c881afa51b7ea0dd1a4b314d29
a mac <aflyingfemme@gmail.com>
2017-10-04 16:40:24 -0700

    Acts as codeable (#412)
    * Add perspective
    
    * Add schema for image coding
    
    * Add form/button image codes
    
    * Add image codes interface
    
    * merge conflicts
    
    * Add tests for comments
    
    * User buffer_update published_text for message content
    
    * Add comments interface
    
    * Fix table
    
    * Finish comment codes interface
    
    * Delete blank files
    
    * Add perspective job
    
    * Fix text sent to perspective api
    
    * move code mapping into its own module
    
    * Add ability to set duplicates
    
    * Remove redundant methods
    
    * Remove console log
    
    * Set duplicates of an image
    
    * Change name of code mapper to codeable
    
    * Set duplicate of images
    
    * Fix set_duplicate for nils
    
    * Make codes as taggable
    
    * delete unnecessary file
    
    * Fix test
    
    * Fix tests
    
    * Fix comment code tests
    
    * Get comment coding to work
    
    * Fix test
    
    * Change code type; remove from comment model
    
    * Fix skip API call test
    
    * Add setting for perspective API
    
    * Add key for perspective API
    
    * Change perspective key name
    
    * Fix image model tests; delete old code
    
    * Fix comment table
    
    * Fix comments loading
    
    * Fix conflicts
    
    * Remove fluff
    
    * Add logger debug
    
    * Fix merge conflicts
    
    * Fix perspective client test to accept comments with quotes in the string
    
    * Change gsub to tr
    
    * Remove newline characters from string comparison
    
    * Add sheets to roo functionality
    
    * Fix conflicts
    
    * Fix squish so that it only runs on published messages
    
    * Fix conflict

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,8 +19,10 @@ class Comment < ActiveRecord::Base
   
   def self.process(filepath)
     comments_spreadsheet = ExcelFileReader.new.read(filepath) if filepath.ends_with?('.xlsx') 
+    #only get messages that were actually published
     messages = Message.where(publish_status: :published_to_social_network)
-    #delete comments to avoid repeats from being saved 
+    #delete comments to avoid repeats from being saved
+    #TODO fix this so it only deletes comments associated with the given experiment
     Comment.destroy_all
     message_index = comments_spreadsheet[0].index("Message")
     comment_index = comments_spreadsheet[0].index("Comment")
@@ -28,16 +30,19 @@ class Comment < ActiveRecord::Base
     comment_username_index = comments_spreadsheet[0].index("Commentator Username")
     orphan_comments = []
     messages.each do |message|  
-      comments_spreadsheet.each do |comments_row| 
-        #some comments have a newline/ space/ carriage return character in the message text that needs to be removed
-        clean_message = comments_row[message_index].squish
-        orphan_comments << clean_message
-        if !message.buffer_update.nil? && message.buffer_update.published_text.squish.include?(clean_message) 
-          message.comments << Comment.create(comment_text: comments_row[comment_index], comment_date: comments_row[comment_date_index], commentator_username: comments_row[comment_username_index])
-          orphan_comments -= [clean_message]
-        end 
+      if !message.buffer_update.nil? 
+        published_text = message.buffer_update.published_text.squish
+        comments_spreadsheet.each do |comments_row| 
+          #some comments have a newline/ space/ carriage return character in the message text that needs to be removed
+          clean_message = comments_row[message_index].squish
+          orphan_comments << clean_message
+          if published_text.include?(clean_message) 
+            message.comments << Comment.create(comment_text: comments_row[comment_index], comment_date: comments_row[comment_date_index], commentator_username: comments_row[comment_username_index])
+            orphan_comments -= [clean_message]
+          end 
+        end
+        message.save
       end
-      message.save
     end
     orphan_comments.each do |comment| 
       logger.debug { "The comment: " + comment + " could not find a matching message." }
