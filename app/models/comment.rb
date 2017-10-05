8c318bf03e8e835ecb936c8e42d884a4f009ffc3
Praveen Angyan <angyan@usc.edu>
2017-10-05 16:28:45 -0700

    Fix comment processing (#418)
    * Fix possible memory leak
    
    * Acts as codeable (#417)
    
    * merge
    
    * Fix merge conflicts
    
    * Display comments
    
    * Removed comment orphan

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -18,13 +18,8 @@ class Comment < ActiveRecord::Base
   belongs_to :message
 
   def self.process(filepath)
-    comments_spreadsheet = ExcelFileReader.new.read(filepath) if filepath.ends_with?('.xlsx') 
-    comments_content = []
+    comments_spreadsheet = ExcelFileReader.new.read(filepath, true) if filepath.ends_with?('.xlsx') 
 
-    comments_spreadsheet.each do |comments_row| 
-      comments_content << comments_row
-    end
-    
     # Only get messages that were actually published
     messages = Message.where(publish_status: :published_to_social_network)
     # Delete comments to avoid repeats from being saved
@@ -34,26 +29,19 @@ class Comment < ActiveRecord::Base
     comment_index = comments_spreadsheet[0].index("Comment")
     comment_date_index = comments_spreadsheet[0].index("Date of Comment")
     comment_username_index = comments_spreadsheet[0].index("Commentator Username")
-    orphan_comments = []
-    
-    
+
     messages.each do |message|  
       if !message.buffer_update.nil? 
         published_text = message.buffer_update.published_text.squish
         comments_content.each do |comments_row| 
           #some comments have a newline/ space/ carriage return character in the message text that needs to be removed
           clean_message = comments_row[message_index].squish
-          orphan_comments << clean_message
           if published_text.include?(clean_message) 
             message.comments << Comment.create(comment_text: comments_row[comment_index], comment_date: comments_row[comment_date_index], commentator_username: comments_row[comment_username_index])
-            orphan_comments -= [clean_message]
           end
         end
         message.save
       end
-    end
-    orphan_comments.each do |comment| 
-      logger.debug { "The comment: " + comment + " could not find a matching message." }
     end
   end
   
