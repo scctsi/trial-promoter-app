6cbc5aa46a024a6b455e7a26e6b3c5de3f4503f0
a mac <aflyingfemme@gmail.com>
2017-10-05 10:27:59 -0700

    Acts as codeable (#415)
    * merge
    
    * Fix merge conflicts

--- spec/lib/perspective_client_spec.rb
+++ spec/lib/perspective_client_spec.rb
@@ -4,15 +4,7 @@ require 'yaml'
 RSpec.describe PerspectiveClient do
   before do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
-<<<<<<< HEAD
-<<<<<<< HEAD
     allow(Setting).to receive(:[]).with(:google_perspective_api_key).and_return(secrets['google_perspective_api_key'])
-=======
-    allow(Setting).to receive(:[]).with(:google_perspective_api_key).and_return(secrets['google_perspective_access_key'])
->>>>>>> acts-as-codeable
-=======
-    allow(Setting).to receive(:[]).with(:google_perspective_api_key).and_return(secrets['google_perspective_api_key'])
->>>>>>> acts-as-codeable
     @text = "This message is stupid."
   end
 
@@ -23,10 +15,8 @@ RSpec.describe PerspectiveClient do
       end
       expect(@toxicity_score).to eq("0.92")
     end
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> acts-as-codeable
+
+
     
     it 'returns the score from the Google Perspective API when comment has double quotes within the string' do
       VCR.use_cassette 'perspective_client/calculate_toxicity_score_for_double_quotes' do
@@ -34,11 +24,8 @@ RSpec.describe PerspectiveClient do
       end
       expect(@toxicity_score).to eq("0.31")
     end
-<<<<<<< HEAD
-=======
->>>>>>> acts-as-codeable
-=======
->>>>>>> acts-as-codeable
+
+
   end
 end 
    
