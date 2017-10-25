41cb8f79fecd9022e560ed0908c507499a49ddb6
Alicia M <aflyingfemme@gmail.com>
2017-10-25 16:44:47 +0000

    Add scripts

---
+++ scripts/add_instagram_comments.rb
@@ -0,0 +1,82 @@
+m = Message.find(11798)
+ m.comments << Comment.create(comment_text: "You kidding me? Hydrogen cyanide is the best part of a cigarette", comment_date: "4/18/2017", commentator_username: "abortedabortion")
+
+m = Message.find(11748)
+ m.comments << Comment.create(comment_text: "smaok iss bud", comment_date: "4/18/2017", commentator_username: "d3ath_wish69")   
+
+m = Message.find(11728)
+ m.comments << Comment.create(comment_text: 'Nobody cares its our choice to do it or not', comment_date: "4/18/2017", commentator_username: "juneboi627")    
+
+
+m = Message.find(11713)
+ m.comments << Comment.create(comment_text: "Tf looks like she's having ceisure", comment_date: "4/18/2017", commentator_username: "damian_sutterlict")
+ m.comments << Comment.create(comment_text: "Why is there a drag queen on a tobacco ad??", comment_date: "4/18/2017", commentator_username: "ozzyphile")
+
+
+m = Message.find(11648)
+ m.comments << Comment.create(comment_text: "Who is this lesbian and how tight is her pussy?", comment_date: "4/18/2017", commentator_username: "thefantasticspastic")  
+
+m = Message.find(11633)
+ m.comments << Comment.create(comment_text: "How do I stop seeing these ads.", comment_date: "4/18/2017", commentator_username: "seanthemlg") 
+ m.comments << Comment.create(comment_text: "Man had sex with a pig.. What you see next will shock you!!", comment_date: "4/18/2017", commentator_username: "spooky_lives_matter") 
+
+m = Message.find(11628)
+ m.comments << Comment.create(comment_text: "Wtf is that", comment_date: "4/18/2017", commentator_username: "gerthy_memes")  
+ m.comments << Comment.create(comment_text: "This makes smoker lungs look good", comment_date: "4/18/2017", commentator_username: "millertimer14")
+ m.comments << Comment.create(comment_text: "Why did you have to put a gay black guy on there", comment_date: "4/18/2017", commentator_username: "william.storie") 
+ m.comments << Comment.create(comment_text: "This dude manicured his head too much", comment_date: "4/18/2017", commentator_username: "pettycrimes")
+
+m = Message.find(11598)
+ m.comments << Comment.create(comment_text: "The smoke weed lol", comment_date: "4/18/2017", commentator_username: "its.marut")  
+
+m = Message.find(11578)
+ m.comments << Comment.create(comment_text: "How are asains gonna make me think against tobacco", comment_date: "4/18/2017", commentator_username: "ozzyphile")   
+
+m = Message.find(11513)
+ m.comments << Comment.create(comment_text: "Or just tell blacks to stop smoking so much", comment_date: "4/18/2017", commentator_username: "cucknado") 
+
+m = Message.find(11503)
+ m.comments << Comment.create(comment_text: "Delete your account nobody cares", comment_date: "4/18/2017", commentator_username: "lordofthesevenseas") 
+ 
+ 
+m = Message.find(11498)
+ m.comments << Comment.create(comment_text: "@not_constantinople this looks like you", comment_date: "4/18/2017", commentator_username: "nicb223") 
+
+m = Message.find(11413)
+ m.comments << Comment.create(comment_text: "Suck my dick", comment_date: "4/18/2017", commentator_username: "lordofthesevenseas")       
+ 
+m = Message.find(11393)
+ m.comments << Comment.create(comment_text: "How bout deaths involving alcohol? Guess u too fraid to touch dat. How lame! How bout da deaths from man made 'medicine' which I could call drugs..Boondocks did a episode on it highlighting the pharma greed running rampant in USA at health expense of U.S. citizens..How bout u tackle tha bigger issues? Tabbacco can't compare to dope epidemic in Maryland an rest of country. Take a seat an shut up if you not gonna highlight the real issues. Facts!", comment_date: "4/18/2017", commentator_username: "ksow014") 
+
+m = Message.find(11378)
+ m.comments << Comment.create(comment_text: "I don't want hair in my lungs ewewew", comment_date: "4/18/2017", commentator_username: "zoom.birb")       
+
+m = Message.find(11348)
+ m.comments << Comment.create(comment_text: "Just smoke crack dudde", comment_date: "4/18/2017", commentator_username: "xanswithaz")    
+ m.comments << Comment.create(comment_text: "@samgiath0ttie or Cannabis", comment_date: "4/18/2017", commentator_username: "typical_tyran")    
+ m.comments << Comment.create(comment_text: "wow thats an aesthetic picture i should use that", comment_date: "4/18/2017", commentator_username: "jackisspookyinside")    
+ 
+m = Message.find(11303)
+ m.comments << Comment.create(comment_text: "L", comment_date: "4/18/2017", commentator_username: "demontamer9001")  
+
+m = Message.find(11248)
+ m.comments << Comment.create(comment_text: "Tobacco is dank", comment_date: "4/18/2017", commentator_username: ".yano.") 
+ 
+m = Message.find(11173)
+ m.comments << Comment.create(comment_text: "Nigga so?", comment_date: "4/18/2017", commentator_username: "shroomtop.j56") 
+ 
+m = Message.find(11168)
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Wtf does this pic have to do with that 🤔🤔🤔", comment_date: "4/18/2017", commentator_username: "blank_circles") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: "4/18/2017", commentator_username: "woodstock__.__") 
+ 
+ 
+ 
+ 
+ 
