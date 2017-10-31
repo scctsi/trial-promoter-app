302a53c60e6df8c9df2a2fdf2314052b7455827d
Alicia M <aflyingfemme@gmail.com>
2017-10-31 20:57:01 +0000

    Fix platform_icon tests

--- spec/models/social_media_profile_spec.rb
+++ spec/models/social_media_profile_spec.rb
@@ -71,12 +71,18 @@ RSpec.describe SocialMediaProfile, type: :model do
   it 'returns the HTML to display a platform icon and name for the platform' do
     social_media_profile = build(:social_media_profile, platform: :twitter)
     
-    expect(social_media_profile.platform_icon_and_name).to eq("<i class = '#{social_media_profile.platform} icon'></i> #{social_media_profile.platform.to_s.titleize}".html_safe)
+    expect(social_media_profile.platform_icon_and_name).to eq("<i class = '#{social_media_profile.platform} icon '></i> #{social_media_profile.platform.to_s.titleize}".html_safe)
   end
 
   it 'returns the HTML to display an icon for the platform' do
     social_media_profile = build(:social_media_profile, platform: :twitter)
     
-    expect(social_media_profile.platform_icon).to eq("<i class = '#{social_media_profile.platform} icon'></i>".html_safe)
+    expect(social_media_profile.platform_icon).to eq("<i class = '#{social_media_profile.platform} icon '></i>".html_safe)
+  end
+  
+  it 'returns the HTML to display a large icon for the platform when size is given as an argument' do
+    social_media_profile = build(:social_media_profile, platform: :twitter)
+    
+    expect(social_media_profile.platform_icon('large')).to eq("<i class = '#{social_media_profile.platform} icon large'></i>".html_safe)
   end
 end
