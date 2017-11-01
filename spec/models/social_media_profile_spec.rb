7fdcfac5df30fee2b04c7d88cad365b8390b3a81
Alicia M <aflyingfemme@gmail.com>
2017-11-01 16:36:22 +0000

    Add test for platform icon and name to take size argument

--- spec/models/social_media_profile_spec.rb
+++ spec/models/social_media_profile_spec.rb
@@ -72,6 +72,12 @@ RSpec.describe SocialMediaProfile, type: :model do
     social_media_profile = build(:social_media_profile, platform: :twitter)
     
     expect(social_media_profile.platform_icon_and_name).to eq("<i class = '#{social_media_profile.platform} icon '></i> #{social_media_profile.platform.to_s.titleize}".html_safe)
+  end  
+  
+  it 'returns the HTML to display a medium-sized platform icon and name for the platform when size is given as an argument' do
+    social_media_profile = build(:social_media_profile, platform: :twitter)
+    
+    expect(social_media_profile.platform_icon_and_name('medium')).to eq("<i class = '#{social_media_profile.platform} icon medium'></i> #{social_media_profile.platform.to_s.titleize}".html_safe)
   end
 
   it 'returns the HTML to display an icon for the platform' do
@@ -80,7 +86,7 @@ RSpec.describe SocialMediaProfile, type: :model do
     expect(social_media_profile.platform_icon).to eq("<i class = '#{social_media_profile.platform} icon '></i>".html_safe)
   end
   
-  it 'returns the HTML to display a large icon for the platform when size is given as an argument' do
+  it 'returns the HTML to display a large-sized icon for the platform when size is given as an argument' do
     social_media_profile = build(:social_media_profile, platform: :twitter)
     
     expect(social_media_profile.platform_icon('large')).to eq("<i class = '#{social_media_profile.platform} icon large'></i>".html_safe)
