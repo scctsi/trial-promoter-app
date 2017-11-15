b6ee215117320d5c07e4281eee642f8c24aeee89
Praveen Angyan <angyan@usc.edu>
2017-11-15 12:24:04 -0800

    Pull in comments automatically for Facebook. (#434)
    * Acts as codeable (#417)
    
    * merge
    
    * Fix merge conflicts
    
    * Display comments
    
    * Removed comment orphan
    
    * Make image codes searchable (#422)
    
    * Fix comment model
    
    * Make image codes searchable
    
    * Fix comments message (#424)
    
    * fix message_id type in comments
    
    * Fix it for real
    
    * Improve coding interface (#426)
    
    * Fix sortable
    
    * Fix platform_icon tests
    
    * Add dividers to dropdowns
    
    * Add dividers to dropdowns
    
    * Remove typo
    
    * Add test for platform icon and name to take size argument
    
    * Fix comment codes (#429)
    
    * Break stuff
    
    * Undo js
    
    * Fix comment codes with pagination
    
    * Comment aggregator (#433)
    
    * Add comment aggregator
    
    * Fix tests with VCR
    
    * Get facebook comments
    
    * Alter schema
    
    * Add process
    
    * Fix merge conflicts take 2
    
    * Delete duplicate code
    
    * Add twitter api tests
    
    * Add facebook comments aggregator
    
    * Fix test fb aggregator
    
    * Fix twitter aggregator test
    
    * Extract methods from getting all facbook comments
    
    * Swap out 'end while' for 'break' statement
    
    * Revert to end while statement
    
    * Fix 'end while' into 'break if' statement
    
    * Add test to check for duplicate comments
    
    * Remove files
    
    * Controller tests (#431)
    
    * Break stuff
    
    * Undo js
    
    * Fix comment codes with pagination
    
    * Finish experiment controller tests for comments page
    
    * Fix typo

--- spec/controllers/experiments_controller_spec.rb
+++ spec/controllers/experiments_controller_spec.rb
@@ -473,4 +473,43 @@ RSpec.describe ExperimentsController, type: :controller do
       expect(@paged_messages).to have_received(:order).with('scheduled_date_time ASC')
     end
   end
+  
+  describe 'GET #comments_page' do
+    before do
+      comments = build_list(:comment, 3)
+      @message = build(:message, comments: comments)
+      @message_comments = double('message_comments')
+      @experiment = create(:experiment)
+      @ordered_comments = []
+      @paged_comments = double('paged_comments')
+      allow(Comment).to receive(:joins).with(:message).and_return(@message_comments)
+      allow(@message_comments).to receive(:page).and_return(@paged_comments)
+      allow(@paged_comments).to receive(:per).and_return(@paged_comments)
+      allow(@paged_comments).to receive(:order).and_return(@ordered_comments)
+    end
+
+    it 'renders generated comments' do
+
+      get :comments_page, id: @experiment.id, page: '2'
+
+      expect(response).to render_template("shared/_comments_page")
+    end
+
+    it 'redirects an unauthorized user' do
+      sign_out(:user)
+
+      get :comments_page, id: @experiment.id
+
+      expect(response).to redirect_to :new_user_session
+    end
+
+    it 'assigns all comments to @comments' do
+      get :comments_page, id: @experiment, page: '2'
+
+      expect(Comment).to have_received(:joins).with(:message)
+      expect(@message_comments).to have_received(:page).with('2')      
+      expect(@paged_comments).to have_received(:per).with(100)
+      expect(@paged_comments).to have_received(:order).with('toxicity_score DESC')
+    end
+  end
 end
