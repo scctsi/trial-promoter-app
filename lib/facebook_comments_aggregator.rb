4a96b57262b8992717d8252a101a93876a669652
Alicia M <aflyingfemme@gmail.com>
2017-11-15 01:23:42 +0000

    Extract methods from getting all facbook comments

--- lib/facebook_comments_aggregator.rb
+++ lib/facebook_comments_aggregator.rb
@@ -11,35 +11,35 @@ class FacebookCommentsAggregator
     return @graph.get_connections("me", "accounts")
   end
 
-  def get_posts(page_id)
-    return @graph.get_object(page_id, fields: ['posts'])
+  def get_paginated_posts(page_id)
+    return @graph.get_connections(page_id, 'posts')
   end
   
-  def get_post_comments(post_id)
+  def get_post_comments(post_id, published_text)
+    post_comments = @graph.get_connections(post_id, "comments", filter: 'stream')
+    begin
+      post_comments.each do |comment|
+        make_comment = Comment.find_or_create_by(social_media_comment_id: comment["id"]) do |new_comment|
+          new_comment.commentator_username = comment["from"]["name"],
+          new_comment.commentator_id = comment["from"]["id"],  
+          new_comment.comment_text = comment["message"]
+        end
+        make_comment.comment_date = make_comment["created_time"]
+        make_comment.message = Message.find_by_published_text(published_text.squish)
+        
+        make_comment.save
+      end
+      post_comments = post_comments.next_page
+    end while post_comments != nil
   end
 
   def get_comments(page_id)
-    get_posts(page_id)
-    all_posts = @graph.get_connections(page_id,"posts")
+    all_posts = get_paginated_posts(page_id)
     begin
-        all_posts.each do |fb_post|
-          all_comments = @graph.get_connections(fb_post["id"], "comments", filter: 'stream')
-          begin
-            all_comments.each do |comment|
-              make_comment = Comment.find_or_create_by(social_media_comment_id: comment["id"]) do |new_comment|
-                new_comment.commentator_username = comment["from"]["name"],
-                new_comment.commentator_id = comment["from"]["id"],  
-                new_comment.comment_text = comment["message"]
-              end
-              make_comment.comment_date = make_comment["created_time"]
-              make_comment.message = Message.find_by_published_text(fb_post["message"].squish)
-              
-              make_comment.save
-            end
-            all_comments = all_comments.next_page
-          end while all_comments != nil
-        end
-        all_posts = all_posts.next_page
+      all_posts.each do |fb_post|
+        get_post_comments(fb_post["id"], fb_post["message"]) 
+      end
+      all_posts = all_posts.next_page
     end while all_posts != nil
   end
 end
