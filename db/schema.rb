1dc0b724796c9fda6caec36bb713b50854531e56
Alicia M <aflyingfemme@gmail.com>
2017-11-30 20:09:38 +0000

    Add twitter replies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108005622) do
+ActiveRecord::Schema.define(version: 20171129204201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20171108005622) do
     t.integer  "message_id"
     t.string   "social_media_comment_id"
     t.string   "commentator_id"
+    t.string   "parent_tweet_id"
   end
 
   create_table "daily_metric_parser_results", force: :cascade do |t|
