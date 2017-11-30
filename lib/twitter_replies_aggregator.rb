90039aa4f32168aa630f402cbfb2e52779da7bf4
Alicia M <aflyingfemme@gmail.com>
2017-11-30 20:09:38 +0000

    Add twitter replies

--- lib/twitter_replies_aggregator.rb
+++ lib/twitter_replies_aggregator.rb
@@ -16,32 +16,68 @@ class TwitterRepliesAggregator
     return @client.user(account).name
   end
 
-  def get_tweets(since_id = nil)
-    options = {}
-    options[:since_id] = since_id unless since_id.nil?
-    tweets = @client.home_timeline(options)
-    CSV.open("twitter_replies.csv", "w+", :headers => ["Date of tweet", "Tweet text", "Id", "Hashtags"], :write_headers => true) do |csv|
-      tweets.each do |tweet|
-        hashtags = []
-        hashtags = tweet.hashtags if tweet.entities?
-          csv << [tweet.created_at, tweet.text, tweet.id, hashtags]
+  def get_comments(handle)
+    tweets = get_all_mentions_for_a_handle
+    comments = []
+
+    tweets.each do |tweet|
+      comment = Comment.find_or_create_by(social_media_comment_id: tweet.id)
+      comment.comment_text = tweet.text
+      comment.commentator_username = tweet.user.name
+      comment.commentator_id = tweet.user.id
+      comment.comment_date = tweet.created_at
+      comment.parent_tweet_id = tweet.in_reply_to_status_id
+      
+      parent_tweet = get_parent_tweet_object(comment.parent_tweet_id)
+      ancestor_tweet = get_ancestor(parent_tweet)
+      comment.message = Message.find_by_alternative_identifier(ancestor_tweet.id) || Message.find_by_alternative_identifier(parent_tweet.text.squish)
+
+      comments << comment
+    end
+    return comments
+  end
+  
+  def get_ancestor(parent_tweet)
+    # If 'in_reply_to_status_id' is nil, then the tweet is the ancestor node; otherwise, get the parent id of that tweet
+    while parent_tweet.in_reply_to_status_id?
+      parent_tweet = get_parent_tweet_object(parent_tweet.in_reply_to_status_id)
+    end
+    return parent_tweet
+  end
+  
+  def get_mentions
+    return @client.mentions_timeline
+  end
+
+  def get_parent_tweet_object(tweet_id)
+    return @client.status(tweet_id)
+  end
+
+  def collect_with_max_id(collection = [], since_id = nil, &block)
+    response = yield(since_id)
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+  
+  def get_all_tweets_for_a_handle(handle)
+    begin
+      collect_with_max_id do |max_id|
+        options = {count: 200, include_rts: true}
+        options[:max_id] = max_id unless max_id.nil?
+        @client.user_timeline(handle, options)
       end
+    rescue Twitter::Error::TooManyRequests => error
+      sleep error.rate_limit.reset_in + 1
+      retry
     end
   end
   
-  # private
-  
-  # def collect_with_since_id(collection=[], since_id=nil, &block)
-  #   response = yield(since_id)
-  #   collection += response
-  #   response.empty? ? collection.flatten : collect_with_since_id(collection, response.last.id - 1, &block)
-  # end
-  
-  # def client.get_recent_tweets(user)
-  #   collect_with_since_id do |since_id|
-  #     options = {count: 200, include_rts: true}
-  #     options[:since_id] = since_id unless since_id.nil?
-  #     user_timeline(user, options)
-  #   end
-  # end
+  def get_all_mentions_for_a_handle
+    begin
+      @client.mentions_timeline
+    rescue Twitter::Error::TooManyRequests => error
+      sleep error.rate_limit.reset_in + 1
+      retry
+    end
+  end  
 end
--- spec/lib/twitter_replies_aggregator_spec.rb
+++ spec/lib/twitter_replies_aggregator_spec.rb
@@ -5,30 +5,66 @@ RSpec.describe TwitterRepliesAggregator do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:twitter).and_return(secrets['twitter'])
     @twitter_replies_aggregator = TwitterRepliesAggregator.new
-    @message = build(:message)
-  #   @message.social_network_id = "980601328736431_1056074954522401"
-
+    @messages = create_list(:message, 3)
+    @messages[2].social_network_id = '931641729844789249'
+    @messages[1].social_network_id = '860207525777559552'
+    @messages.each{|message| message.save }
+    @messages[0].buffer_update = create(:buffer_update, published_text: "Every day in the US 700+ youth become daily smokers. Nicotine is highly addictive. https://t.co/IyOA8GwASQ #tobacco https://t.co/81U9DrHpU0")
+    @messages[1].buffer_update = create(:buffer_update, published_text: "Polonium-210 is a chemical in nuclear reactors. It’s also found in #cigarette smoke. https://t.co/y9vTJ05EVU https://t.co/PWhxTazzzs")
+    @messages[2].buffer_update = create(:buffer_update, published_text: "location, location, location!")
     VCR.use_cassette 'twitter_replies_aggregator/test_setup' do
       @handle = @twitter_replies_aggregator.get_account('BeFreeOfTobacco')
+    end
+    
+    VCR.use_cassette 'twitter_replies_aggregator/get_all_tweets_for_a_handle' do
+      @tweets = @twitter_replies_aggregator.get_all_tweets_for_a_handle('BeFreeOfTobacco')
     end
   end
 
   describe "(development only tests)", :development_only_tests => true do
+    
     it 'gets an account handle' do
       
       expect(@handle).to eq("BeFreeOfTobacco")
     end
-  
-    it 'gets the timeline of the main account' do
-      VCR.use_cassette 'twitter_replies_aggregator/get_tweets' do
-          tweets = @twitter_replies_aggregator.get_tweets
-          file_lines_count_first = CSV.read("twitter_replies.csv").count
-          @twitter_replies_aggregator.get_tweets
-          file_lines_count_second = CSV.read("twitter_replies.csv").count
-  
-          expect(tweets.count).to eq(4)
-          expect(file_lines_count_first).to eq(5)
-          expect(file_lines_count_first).to eq(file_lines_count_second)
+    
+    it 'gets all the tweets' do
+      VCR.use_cassette 'twitter_replies_aggregator/get_all_tweets_for_a_handle' do
+        tweets = @twitter_replies_aggregator.get_all_tweets_for_a_handle('BeFreeOfTobacco')
+
+        expect(tweets.count).to eq(280)
+      end
+    end
+    
+    it 'gets the parent tweet for a comment' do
+      VCR.use_cassette 'twitter_replies_aggregator/status' do
+        parent_tweet_id = '931668280883871744'
+        
+        tweet = @twitter_replies_aggregator.get_parent_tweet_object(parent_tweet_id)
+      
+        expect(tweet.text).to eq("this is a test reply.")
+        expect(tweet.user.name).to eq("BeFreeOfTobacco")
+      end
+    end
+    
+    it 'gets the comments for all tweets' do
+      VCR.use_cassette 'twitter_replies_aggregator/get_comments' do
+        comments = @twitter_replies_aggregator.get_comments("BeFreeOfTobacco")
+
+        expect(comments.count).to eq(6)
+        expect(comments[0]['comment_text']).to eq("@BeFreeOfTobacco so many replies")
+        expect(comments[0]['parent_tweet_id']).to eq("931668280883871744")
+        expect(comments[0].message.social_network_id).to eq("931641729844789249")
+        expect(comments[0].message.buffer_update.published_text).to eq("location, location, location!")
+        expect(comments[4]['comment_text']).to eq("@BeFreeOfTobacco @SmokersMatch LOL, doubtful")
+        expect(comments[4]['parent_tweet_id']).to eq("864963744425750531")
+        expect(comments[4].message.social_network_id).to eq("860207525777559552")
+        expect(comments[4].message.buffer_update.published_text).to eq("Polonium-210 is a chemical in nuclear reactors. It’s also found in #cigarette smoke. https://t.co/y9vTJ05EVU https://t.co/PWhxTazzzs")
+        expect(comments[5]['comment_text']).to eq("@BeFreeOfTobacco https://t.co/zbPl4ZFCg3")
+        expect(comments[5]['parent_tweet_id']).to eq("860207525777559552")
+        expect(comments[5].message.social_network_id).to eq("860207525777559552")
+        expect(comments[5].message.buffer_update.published_text).to eq("Polonium-210 is a chemical in nuclear reactors. It’s also found in #cigarette smoke. https://t.co/y9vTJ05EVU https://t.co/PWhxTazzzs")
+      
       end
     end
   end
