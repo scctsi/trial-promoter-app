c2b84046588e3de19bc7ea415da53a7814bfe283
Alicia M <aflyingfemme@gmail.com>
2017-12-06 22:28:12 +0000

    trash

--- lib/facebook_ads_client.rb
+++ lib/facebook_ads_client.rb
@@ -10,4 +10,23 @@ class FacebookAdsClient
     @account = FacebookAds::AdAccount.get(account, @session) 
     return @account
   end
+  
+  def get_campaign_names
+    return @account.campaigns(fields: 'name, comments').map(&:name)
+  end  
+  
+  def get_all_campaign_ids
+    return @account.campaigns(fields: 'id').map(&:id)
+  end
+  
+  def get_ads(campaign_id)
+    return @account.get(campaign_id: campaign_id, fields: 'ads')
+  end
+  
+  def get_comments(object_id)
+    @account.reload!
+    # return @account.get(field: 'me', 'comments')
+    campaign = @account.get(object_id)
+    return campaign
+  end
 end
--- spec/lib/facebook_ads_client_spec.rb
+++ spec/lib/facebook_ads_client_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe FacebookAdsClient do
   before do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:facebook_access_token).and_return(secrets['facebook_access_token'])
-    # allow(Setting).to receive(:[]).with(:facebook_app_secret).and_return(secrets['facebook_app_secret'])
+    allow(Setting).to receive(:[]).with(:facebook_app_secret).and_return(secrets['facebook_app_secret'])
     @facebook_ads_client = FacebookAdsClient.new
     VCR.use_cassette 'facebook_ads_client/test_setup' do
       @ad_account = @facebook_ads_client.get_account('act_114516875546999')
@@ -17,7 +17,45 @@ RSpec.describe FacebookAdsClient do
       expect(@ad_account).not_to be_nil
       expect(@ad_account["id"]).to eq("act_114516875546999")
       expect(@ad_name).to eq("Tommy Trogan")
+    end
     
+    it 'gets the campaign names associated with an ad account' do
+      VCR.use_cassette 'facebook_ads_client/get_campaign_names' do
+        campaign_names = @facebook_ads_client.get_campaign_names
+        
+        expect(campaign_names.count).to eq(732)
+        expect(campaign_names.first).to eq("Post: \"Hydrogen cyanide is found in rat poison. It’s...\"")
+        # p campaign_names
+        # expect(campaign_names.first).to include("6076509762239")
+      end
+    end
+    
+    it 'gets the campaign ids' do
+      VCR.use_cassette 'facebook_ads_client/get_all_campaign_ids' do
+        all_campaign_ids = @facebook_ads_client.get_all_campaign_ids
+
+        expect(all_campaign_ids.count).to eq(732)
+      end
+    end
+    
+    it 'gets the ads from a campaign id' do
+      VCR.use_cassette 'facebook_ads_client/get_ads' do
+        campaign_id = "6076509762239"
+
+        ads = @facebook_ads_client.get_ads(campaign_id)
+
+        expect(ads.body["ads"]).to eq("ads")
+      end
+    end
+    
+    it 'gets the comments from an ad id' do
+      VCR.use_cassette 'facebook_ads_client/get_comments' do
+        ad_id = "6075124407239"
+        
+        comments = @facebook_ads_client.get_comments(ad_id)
+        
+        expect(comments).to eq("something")
+      end
     end
   end
 end
