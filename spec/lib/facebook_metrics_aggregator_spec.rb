321db5f0b66dcdf39b77631e2b4f06bf924fca8a
Alicia M <aflyingfemme@gmail.com>
2018-01-06 01:25:54 +0000

    Add job; additional tests

--- spec/lib/facebook_metrics_aggregator_spec.rb
+++ spec/lib/facebook_metrics_aggregator_spec.rb
@@ -23,34 +23,61 @@ RSpec.describe FacebookMetricsAggregator do
       expect(@page["name"]).to eq("B Free of Tobacco")
     end
     
-    it 'gets the impressions for an individual post' do
-      post_ids = ["980601328736431_1009727572490473", "980601328736431_1056074954522401", "980601328736431_1060154484114448"]
+    it 'gets the lifetime impressions for a single post' do
+      post_id = "980601328736431_1009727572490473"
 
       VCR.use_cassette 'facebook_metrics_aggregator/get_post_impressions' do
-        impressions = []
         
-        post_ids.each do |post_id|
-          impressions << @facebook_metrics_aggregator.get_post_impressions(@page["id"], post_id)
-        end
-        
-        expect(impressions[0]["likes"]).to eq(19)
-        expect(impressions[0]["comments"].count).to eq(1)
-        expect(impressions[0]["shares"]).to eq(nil)
-        expect(impressions[1]["likes"]).to eq(25)
-        expect(impressions[1]["comments"].count).to eq(24)
-        expect(impressions[1]["shares"].count).to eq(3)
-        expect(impressions[2]["likes"]).to eq(25)
-        expect(impressions[2]["comments"].count).to eq(3)
-        expect(impressions[2]["shares"]).to eq(nil)
+        impressions = @facebook_metrics_aggregator.get_post_impressions(@page["id"], post_id)
+      
+        expect(impressions).to eq(502)
       end
     end
     
-    it 'gets posts for the page B Free of Tobacco' do
-      VCR.use_cassette 'facebook_metrics_aggregator/get_posts' do
-        posts = @facebook_metrics_aggregator.get_posts(@page["id"])
+    it 'gets the impressions for an individual post' do
+      post_ids = ["980601328736431_1009727572490473", "980601328736431_1056074954522401", "980601328736431_1060154484114448"]
 
-        expect(posts[0][0]["message"]).to eq("#Smoking can weaken your immune system, leaving you more vulnerable to bronchitis & pneumonia. http://bit.ly/2sHRInS")
-        expect(posts[0][0]["id"]).to eq("980601328736431_1061544820642081")
+      VCR.use_cassette 'facebook_metrics_aggregator/get_post_metrics' do
+        metrics = []
+        
+        post_ids.each do |post_id|
+          metrics << @facebook_metrics_aggregator.get_post_metrics(@page["id"], post_id)
+        end
+        expect(metrics[0]["likes"]).to eq(19)
+        expect(metrics[0]["comments"].count).to eq(1)
+        expect(metrics[0]["clicks"][0]["values"][0]["value"]["other clicks"]).to eq(3)
+        expect(metrics[0]["clicks"][0]["values"][0]["value"]["photo view"]).to eq(9)
+        expect(metrics[0]["shares"]).to eq(0)
+        expect(metrics[1]["likes"]).to eq(25)
+        expect(metrics[1]["comments"].count).to eq(24)
+        expect(metrics[1]["clicks"][0]["values"][0]["value"]["other clicks"]).to eq(90)
+        expect(metrics[1]["clicks"][0]["values"][0]["value"]["photo view"]).to eq(105)
+        expect(metrics[1]["shares"]).to eq(3)
+        expect(metrics[2]["likes"]).to eq(25)
+        expect(metrics[2]["comments"].count).to eq(3)
+        expect(metrics[2]["clicks"][0]["values"][0]["value"]["other clicks"]).to eq(10)
+        expect(metrics[2]["clicks"][0]["values"][0]["value"]["photo view"]).to eq(9)
+        expect(metrics[2]["shares"]).to eq(0)
+      end
+    end
+    
+    it 'gets posts for the page B Free of Tobacco on a specific day' do
+      VCR.use_cassette 'facebook_metrics_aggregator/get_posts' do
+        posts_day = @facebook_metrics_aggregator.get_posts(@page["id"], Date.new(2017, 4, 19))
+
+        @messages.each{|m| m.reload }
+        
+        expect(@messages[0].metrics[0]["data"]).to include(:shares=>0, :likes=>25)
+        expect(@messages[1].metrics[0]["data"]).to include(:shares=>0, :likes=>19)
+        expect(@messages[2].metrics[0]).to be(nil)
+        expect(@messages[0].impressions_by_day).to eq({ Date.new(2017, 4, 19) => 1514 })
+        expect(@messages[1].impressions_by_day).to eq({ Date.new(2017, 4, 19) => 502 })
+        expect(@messages[2].impressions_by_day).to eq({})
+        expect(posts_day.count).to eq(12)
+        expect(posts_day[0][0]["message"]).to eq("#Smoking can weaken your immune system, leaving you more vulnerable to bronchitis & pneumonia. http://bit.ly/2sHRInS")
+        expect(posts_day[1][0]["message"]).to eq("About 30% of US cancer deaths are linked to #smoking. Smoking can cause cancer almost anywhere in the body. http://bit.ly/2tlJhxv")
+        expect(posts_day[0][0]["id"]).to eq("980601328736431_1061544820642081")
+        expect(posts_day[1][0]["id"]).to eq("980601328736431_1055617114568185")
       end
     end
   
@@ -59,6 +86,7 @@ RSpec.describe FacebookMetricsAggregator do
 
         posts = @facebook_metrics_aggregator.get_paginated_posts(@page["id"], "2017-04-20", "2017-04-21")
 
+        expect(posts.count).to eq(3)
         expect(posts.first["message"]).to eq( "Even occasional #smoking can hurt you. On average, every cig reduces your life by 11 minutes. http://bit.ly/2oq3WdA")
         expect(posts.first["id"]).to eq("980601328736431_1008353069294590")
       end
@@ -66,9 +94,9 @@ RSpec.describe FacebookMetricsAggregator do
     
     it 'does not add repeat comments' do
       VCR.use_cassette 'facebook_metrics_aggregator/get_double_post_comments' do
-        posts = @facebook_metrics_aggregator.get_paginated_posts(@page["id"])
+        paginated_posts = @facebook_metrics_aggregator.get_paginated_posts(@page["id"])
 
-        expect(posts.count).to eq(25)
+        expect(paginated_posts.count).to eq(25)
       end
     end
   end
