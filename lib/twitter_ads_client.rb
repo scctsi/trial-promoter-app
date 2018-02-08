85a89e9f6982f4a8181a3c5f10d50fd44f262b83
Alicia M <aflyingfemme@gmail.com>
2018-02-08 00:32:02 +0000

    Add functionality for building campaigns; tests

--- lib/twitter_ads_client.rb
+++ lib/twitter_ads_client.rb
@@ -1,56 +1,85 @@
 class TwitterAdsClient
-  require 'oauth2'
-  API_VERSION = '3.0'
-  
   def initialize(sandbox = false)
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     Setting[:twitter] = secrets['twitter']
     
-    @twitter_ads_client = TwitterAds::Client.new(
+    @client = TwitterAds::Client.new(
       Setting[:twitter]["consumer_key"],
       Setting[:twitter]["consumer_secret"],
       Setting[:twitter]["access_token"],
       Setting[:twitter]["access_token_secret"],
       sandbox: sandbox
     )
-    
-    # @client = OAuth2::Client.new( 
-    #   '@BeFreeOfTobacco',
-    #   Setting[:twitter]["consumer_key"],
-    #   # Setting[:twitter]["consumer_secret"],
-    #   site: 'https://ads-api-sandbox.twitter.com/3/accounts?account_ids=gq1axo'
-    #   # Setting[:twitter]["access_token"],
-    #   # Setting[:twitter]["access_token_secret"],
-    #   # sandbox: sandbox 
-    # )
   end
     
   def get_account
-    @twitter_ads_client.accounts('gq1axo')
-    # @client.auth_code.authorize_url(:redirect_uri => 'http://0.0.0.0:3000/oauth2/callback')
-    # p @client.auth_code
-    # token = @client.auth_code.get_token('authorization_code', :redirect_uri => 'http://0.0.0.0:3000/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
-
+    return @client.accounts
   end
   
   def get_campaigns(ad_account_id)
-    account = get_account
-    resource = "/TwitterAds::3.0/accounts/#{account.first.id}/features"
-    
-    account.first.campaigns
-  end
-
-  def create_campaign
-    account = @client.create_list(
-      "params":{
-      "start_time":"2017-07-10",
-      "name":"batch campaigns",
-      "funding_instrument_id":"lygyi",
-      "daily_budget_amount_local_micro":140000000,
-      "entity_status":"PAUSED"
-    })
+    return @client.accounts(ad_account_id).campaigns
   end
   
-  def delete_campaign
+  def get_campaign(ad_account_id, campaign_id)
+    return @client.accounts(ad_account_id).campaigns(campaign_id)
+  end
+  
+  def get_line_item(ad_account_id, line_item_id)
+    return @client.accounts(ad_account_id).line_items(line_item_id)
+  end
+  
+  def get_targeting_criteria(ad_account_id, line_item_id, targeting_criteria_id)
+    return @client.accounts(ad_account_id).line_items(line_item_id).targeting_criteria(targeting_criteria_id)
+  end
+
+  def create_campaign(account, campaign_params = {})
+    campaign = TwitterAds::Campaign.new(account)
+    campaign.funding_instrument_id = account.funding_instruments.first.id
+    campaign.total_budget_amount_local_micro = campaign_params[:total_budget_amount_local_micro]
+    campaign.daily_budget_amount_local_micro = campaign_params[:daily_budget_amount_local_micro]
+    campaign.name = campaign_params[:name]
+    campaign.entity_status = campaign_params[:entity_status]
+    
+    campaign.start_time = campaign_params[:start_time]
+    campaign.save
+    
+    return campaign
+  end
+  
+  def create_line_item(ad_account, campaign_id, line_item_params = {})
+    line_item = TwitterAds::LineItem.new(ad_account)
+    line_item.campaign_id = campaign_id
+    line_item.name = line_item_params[:name]
+    line_item.product_type = line_item_params[:product_type]
+    line_item.placements = line_item_params[:placements]
+    line_item.objective = line_item_params[:objective]
+    line_item.bid_type = line_item_params[:bid_type]
+    line_item.entity_status = line_item_params[:entity_status]
+    line_item.save
+  end
+
+  def create_targeting_criteria(ad_account, line_item, targeting_criteria_params = {})
+    targeting_criteria = TwitterAds::TargetingCriteria.new(ad_account)
+    targeting_criteria.line_item_id = line_item.id
+    targeting_criteria.targeting_type = targeting_criteria_params[:targeting_type]
+    targeting_criteria.location_type = targeting_criteria_params[:location_type]
+    targeting_criteria.targeting_value = targeting_criteria_params[:targeting_value]
+    targeting_criteria.save
+  end
+  
+  def pause_campaign(campaign)
+    campaign.entity_status = 'PAUSED'
+    campaign.save
+  end
+  
+  def delete_campaign(campaign)
+    campaign.delete!
+  end
+  
+  def get_organic_tweets
+    
+  end
+  
+  def promote_tweet 
   end
 end
--- spec/lib/twitter_ads_client_spec.rb
+++ spec/lib/twitter_ads_client_spec.rb
@@ -6,30 +6,124 @@ RSpec.describe TwitterAdsClient do
     secrets = YAML.load_file("#{Rails.root}/spec/secrets/secrets.yml")
     allow(Setting).to receive(:[]).with(:twitter).and_return(secrets['twitter'])
     @twitter_ads_client = TwitterAdsClient.new(true)
+    # campaign = nil
+    VCR.use_cassette 'twitter_ads_client/test_setup' do
+      @account = @twitter_ads_client.get_account.first
+    end
   end
 
   describe "(development only tests)", :development_only_tests => true do
     it 'gets the account' do
-      VCR.use_cassette 'twitter_ads_client/test_setup' do
-        account = @twitter_ads_client.get_account
-  p account      
-        expect(account.first).to include("18ce53zp9m8")
-        expect(account.first.name).to include("USC Clinical Trials")
-      end
+      expect(@account.id).to include("gq1azc")
+      expect(@account.name).to include("Sandbox account for @USCTrials")
     end
   
-    xit 'gets the campaigns for the ad account' do
+    it 'gets any campaigns associated with the ad account' do
       VCR.use_cassette 'twitter_ads_client/get_campaigns' do
-        campaigns = @twitter_ads_client.get_campaigns('18ce53zp9m8')
+        @campaigns = @twitter_ads_client.get_campaigns(@account.id)
 
-        expect(campaigns.count).to be(0)
+        expect(@campaigns.count).to be(115)
+      end
+    end
+      
+    it 'creates a campaign associated with the ad account' do
+      VCR.use_cassette 'twitter_ads_client/create_campaign' do
+        campaign_params = {
+          total_budget_amount_local_micro: 1_000_000_000,
+          daily_budget_amount_local_micro: 1_000_000,
+          name: 'my first campaign',
+          entity_status: 'ACTIVE',
+          start_time: Time.new(2018, 2, 6, 2, 2, "+00:00")
+        }
+        
+        campaign = @twitter_ads_client.create_campaign(@account, campaign_params)
+        
+        expect(campaign.id).to include("hqbn")
+        expect(campaign.name).to include("my first campaign")
+        expect(campaign.entity_status).to include("ACTIVE")
+        
+        VCR.use_cassette 'twitter_ads_client/create_line_item' do
+          line_item_params = {
+            name: 'my first objective',
+            product_type: 'PROMOTED_TWEETS',
+            placements: 'ALL_ON_TWITTER',
+            objective: 'AWARENESS',
+            bid_type: 'AUTO',
+            entity_status: 'ACTIVE'
+          }
+          
+          @line_item = @twitter_ads_client.create_line_item(@account, campaign.id, line_item_params)
+        
+          expect(@line_item.name).to include("my first objective")
+          expect(@line_item.id).to include("ek2k")
+        end
+        
+        VCR.use_cassette 'twitter_ads_client/create_targeting_criteria' do
+          targeting_criteria_params = {      
+            targeting_type: 'LOCATION',
+            location_type: 'REGIONS',
+            targeting_value: 'fbd6d2f5a4e4a15e'
+          }
+          
+          @targeting_value = @twitter_ads_client.create_targeting_criteria(@account, @line_item, targeting_criteria_params)
+          
+          expect(@targeting_value.id).to include("38r08")
+        end
       end
     end
 
-    xit 'creates a new campaign' do
-      VCR.use_cassette 'twitter_ads_client/create_campaign' do
-        @twitter_ads_client.create_campaign
+    it 'deletes a campaign' do
+      VCR.use_cassette 'twitter_ads_client/get_campaigns' do
+        @campaigns = @twitter_ads_client.get_campaigns(@account.id)
+
+        expect(@campaigns.count).to be(115)
       end
+
+      VCR.use_cassette 'twitter_ads_client/delete_campaign' do
+        @twitter_ads_client.delete_campaign(@campaigns.last)
+        campaigns = @twitter_ads_client.get_campaigns(@account.id)
+        
+        expect(campaigns.count).to eq(114)
+      end
+    end
+    
+    it 'gets a campaign' do
+      VCR.use_cassette 'twitter_ads_client/get_campaign' do
+        campaign_id = "hqac"
+        campaign = @twitter_ads_client.get_campaign(@account.id, campaign_id)
+        
+        expect(campaign.name).to eq('my first campaign')
+      end
+    end
+    
+    it 'gets a line_item' do
+      VCR.use_cassette 'twitter_ads_client/get_line_item' do
+        line_item_id = "ek2g"
+        line_item = @twitter_ads_client.get_line_item(@account.id, line_item_id)
+        
+        expect(line_item.name).to eq('my first objective')
+      end
+    end
+    
+    it 'gets targeting_criteria' do
+      VCR.use_cassette 'twitter_ads_client/get_targeting_criteria' do
+        line_item_id = "ek2g"
+        targeting_criteria_id = "38r06"
+        targeting_criteria = @twitter_ads_client.get_targeting_criteria(@account.id, line_item_id, targeting_criteria_id)
+        
+        expect(targeting_criteria.targeting_type).to eq('LOCATION')
+      end
+    end
+    
+    it 'updates a campaign' do
+      VCR.use_cassette 'twitter_ads_client/update_campaign' do
+        campaign_id = "hqbn"
+        campaign = @twitter_ads_client.get_campaign(@account.id, campaign_id)
+        expect(campaign.entity_status).to eq('ACTIVE')
+
+        @twitter_ads_client.pause_campaign(campaign)
+        expect(campaign.entity_status).to eq('PAUSED')
+      end      
     end
   end
 end
