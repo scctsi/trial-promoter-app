e5a221208a89519bc618c54205f9e3ecad365f78
Alicia M <aflyingfemme@gmail.com>
2018-02-20 19:50:50 +0000

    add arguments; TODO finish mocking arguments

--- app/jobs/promote_twitter_messages_job.rb
+++ app/jobs/promote_twitter_messages_job.rb
@@ -4,6 +4,6 @@ class PromoteTwitterMessagesJob < ActiveJob::Base
   def perform
     @twitter_ads_client = TwitterAdsClient.new
     @account = @twitter_ads_client.get_account
-    @twitter_ads_client.promote_tweet
+    @twitter_ads_client.promote_tweet(@account, line_item_id, tweet_id)
   end
 end
--- spec/jobs/promote_twitter_messages_job_spec.rb
+++ spec/jobs/promote_twitter_messages_job_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe PromoteTwitterMessagesJob, type: :job do
     allow(TwitterAdsClient).to receive(:new)
     @twitter_ads_client = TwitterAdsClient.new
     allow(@twitter_ads_client).to receive(:get_account)
-    allow(@twitter_ads_client).to receive(:promote_tweet)
+    allow(@twitter_ads_client).to receive(:promote_tweet).with(:account, :line_item_id, :tweet_id)
   end
 
   it 'queues the job' do
