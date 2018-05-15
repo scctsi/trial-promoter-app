36c4312ffd65daadcbd3f35c03c13e48050ce000
Praveen Angyan <angyan@usc.edu>
2018-05-15 13:00:55 -0700

    Seed comments (#441) (#442)
    * Add scripts
    
    * Add some comments to instagram messages
    
    * Add instagram comments
    
    * Pull in comments automatically for Facebook. (#434)
    
    * Acts as codeable (#417)
    
    * merge
    
    * Fix merge conflicts
    
    * Display comments
    
    * Removed comment orphan
    
    * Make image codes searchable (#422)
    
    * Fix comment model
    
    * Make image codes searchable
    
    * Fix comments message (#424)
    
    * fix message_id type in comments
    
    * Fix it for real
    
    * Improve coding interface (#426)
    
    * Fix sortable
    
    * Fix platform_icon tests
    
    * Add dividers to dropdowns
    
    * Add dividers to dropdowns
    
    * Remove typo
    
    * Add test for platform icon and name to take size argument
    
    * Fix comment codes (#429)
    
    * Break stuff
    
    * Undo js
    
    * Fix comment codes with pagination
    
    * Comment aggregator (#433)
    
    * Add comment aggregator
    
    * Fix tests with VCR
    
    * Get facebook comments
    
    * Alter schema
    
    * Add process
    
    * Fix merge conflicts take 2
    
    * Delete duplicate code
    
    * Add twitter api tests
    
    * Add facebook comments aggregator
    
    * Fix test fb aggregator
    
    * Fix twitter aggregator test
    
    * Extract methods from getting all facbook comments
    
    * Swap out 'end while' for 'break' statement
    
    * Revert to end while statement
    
    * Fix 'end while' into 'break if' statement
    
    * Add test to check for duplicate comments
    
    * Remove files
    
    * Controller tests (#431)
    
    * Break stuff
    
    * Undo js
    
    * Fix comment codes with pagination
    
    * Finish experiment controller tests for comments page
    
    * Fix typo
    
    * Add gemfile.lock
    
    * Add remaining instagram comments ad/organic
    
    * Fix significant digits
    
    * Add seeds for instagram and twitter comments
    
    * Add scripts
    
    * Add some comments to instagram messages
    
    * Add instagram comments
    
    * Add remaining instagram comments ad/organic
    
    * Add seeds for instagram and twitter comments

---
+++ scripts/add_instagram_comments.rb
@@ -0,0 +1,201 @@
+m = Message.find(11798)
+ m.comments << Comment.create(comment_text: "You kidding me? Hydrogen cyanide is the best part of a cigarette", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "abortedabortion")
+m.save
+
+m = Message.find(11748)
+ m.comments << Comment.create(comment_text: "smaok iss bud", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "d3ath_wish69")   
+m.save
+
+m = Message.find(11728)
+ m.comments << Comment.create(comment_text: 'Nobody cares its our choice to do it or not', comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "juneboi627")    
+m.save
+
+
+m = Message.find(11713)
+ m.comments << Comment.create(comment_text: "Tf looks like she's having ceisure", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "damian_sutterlict")
+ m.comments << Comment.create(comment_text: "Why is there a drag queen on a tobacco ad??", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "ozzyphile")
+m.save
+
+
+m = Message.find(11648)
+ m.comments << Comment.create(comment_text: "Who is this lesbian and how tight is her pussy?", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "thefantasticspastic")  
+m.save
+
+m = Message.find(11633)
+ m.comments << Comment.create(comment_text: "How do I stop seeing these ads.", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "seanthemlg") 
+ m.comments << Comment.create(comment_text: "Man had sex with a pig.. What you see next will shock you!!", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "spooky_lives_matter") 
+m.save
+
+m = Message.find(11628)
+ m.comments << Comment.create(comment_text: "Wtf is that", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "gerthy_memes")  
+ m.comments << Comment.create(comment_text: "This makes smoker lungs look good", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "millertimer14")
+ m.comments << Comment.create(comment_text: "Why did you have to put a gay black guy on there", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "william.storie") 
+ m.comments << Comment.create(comment_text: "This dude manicured his head too much", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "pettycrimes")
+m.save
+
+m = Message.find(11598)
+ m.comments << Comment.create(comment_text: "The smoke weed lol", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "its.marut")  
+m.save
+
+m = Message.find(11578)
+ m.comments << Comment.create(comment_text: "How are asains gonna make me think against tobacco", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "ozzyphile")   
+m.save
+
+m = Message.find(11513)
+ m.comments << Comment.create(comment_text: "Or just tell blacks to stop smoking so much", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "cucknado") 
+m.save
+
+m = Message.find(11503)
+ m.comments << Comment.create(comment_text: "Delete your account nobody cares", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "lordofthesevenseas") 
+m.save
+ 
+ 
+m = Message.find(11498)
+ m.comments << Comment.create(comment_text: "@not_constantinople this looks like you", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "nicb223") 
+m.save
+
+m = Message.find(11413)
+ m.comments << Comment.create(comment_text: "Suck my dick", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "lordofthesevenseas")       
+m.save
+ 
+m = Message.find(11393)
+ m.comments << Comment.create(comment_text: "How bout deaths involving alcohol? Guess u too fraid to touch dat. How lame! How bout da deaths from man made 'medicine' which I could call drugs..Boondocks did a episode on it highlighting the pharma greed running rampant in USA at health expense of U.S. citizens..How bout u tackle tha bigger issues? Tabbacco can't compare to dope epidemic in Maryland an rest of country. Take a seat an shut up if you not gonna highlight the real issues. Facts!", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "ksow014") 
+m.save
+
+m = Message.find(11378)
+ m.comments << Comment.create(comment_text: "I don't want hair in my lungs ewewew", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "zoom.birb")       
+m.save
+
+m = Message.find(11348)
+ m.comments << Comment.create(comment_text: "Just smoke crack dudde", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "xanswithaz")    
+ m.comments << Comment.create(comment_text: "@samgiath0ttie or Cannabis", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "typical_tyran")    
+ m.comments << Comment.create(comment_text: "wow thats an aesthetic picture i should use that", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "jackisspookyinside")    
+m.save
+ 
+m = Message.find(11303)
+ m.comments << Comment.create(comment_text: "L", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "demontamer9001")  
+m.save
+
+m = Message.find(11248)
+ m.comments << Comment.create(comment_text: "Tobacco is dank", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: ".yano.") 
+m.save
+ 
+m = Message.find(11173)
+ m.comments << Comment.create(comment_text: "Nigga so?", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "shroomtop.j56") 
+m.save
+ 
+m = Message.find(11168)
+ m.comments << Comment.create(comment_text: "Smoke weed instead", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "woodstock__.__") 
+ m.comments << Comment.create(comment_text: "Wtf does this pic have to do with that 🤔🤔🤔", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username:  "blank_circles") 
+ m.comments << Comment.create(comment_text: "If it was a straight couple you wouldn't have commented @blank_circles", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "this_is_cuz_im_black") 
+ m.comments << Comment.create(comment_text: "@this_is_cuz_im_black I would've still commented tf u mean", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "blank_circles") 
+ m.comments << Comment.create(comment_text: "Lmao @blank_circles", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "this_is_cuz_im_black") 
+ m.comments << Comment.create(comment_text: "Seriously what does this picture have anything to do with smoking and cancer lmao", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "liz_____p") 
+ m.comments << Comment.create(comment_text: "I was gonna make my usual 'gay' comment but there is actually a gay couple. So I give you this instead:", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "17stevenmichael08") 
+ m.comments << Comment.create(comment_text: "L", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "17stevenmichael08") 
+m.save
+
+m = Message.find(11133)
+ m.comments << Comment.create(comment_text: "Lol", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "doctor.penn") 
+m.save
+
+m = Message.find(11123)
+ m.comments << Comment.create(comment_text: "Unless its weed. No ones snoked a joint and was still stressed", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "woodstock__.__")
+m.save
+ 
+m = Message.find(11113)
+ m.comments << Comment.create(comment_text: "Man ive lost months at this point LOL", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "schmolhighwaters")
+ m.comments << Comment.create(comment_text: "How are people supposed to stop when you put motivating messages like these on Instagram", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "edgydiabetic")
+m.save
+  
+m = Message.find(11098)
+ m.comments << Comment.create(comment_text: "@xxkellymarie_xx", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "hannah_and_zack") 
+ m.comments << Comment.create(comment_text: "someone help he calculate the number of cigs I have to smoke so I can die", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "flip__heck")
+m.save
+   
+m = Message.find(11033)
+ m.comments << Comment.create(comment_text: "@xxkellymarie_xx", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "hannah_and_zack") 
+ m.comments << Comment.create(comment_text: "someone help he calculate the number of cigs I have to smoke so I can die", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "flip__heck")
+m.save
+ 
+m = Message.find(10998)
+ m.comments << Comment.create(comment_text: "#FatBackwoodsOnly", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "dabtanium_studios") 
+m.save
+ 
+m = Message.find(10973)
+ m.comments << Comment.create(comment_text: "I hope I get cancer from smoking", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "backwoodtripgod") 
+m.save
+
+m = Message.find(10893)
+ m.comments << Comment.create(comment_text: "im gay", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "rattyonce") 
+m.save
+  
+m = Message.find(10858)
+ m.comments << Comment.create(comment_text: "😮😧😱what no way!", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "matt_barton_0614") 
+ m.comments << Comment.create(comment_text: "thought this was mrs. marquez @laylita.xo", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "meiiilaaani") 
+m.save
+  
+m = Message.find(10808)
+ m.comments << Comment.create(comment_text: "Not everyone that smokes cigs gets cancer fam😂or any problems!", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "aiden.hill1125") 
+m.save
+
+m = Message.find(10798)
+ m.comments << Comment.create(comment_text: "Thats why you should smoke weed", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "spookskvll.jpg") 
+m.save
+
+m = Message.find(10733)
+ m.comments << Comment.create(comment_text: "He is gay", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "eric9862") 
+ m.comments << Comment.create(comment_text: "this picture has nothing to do with smoking in fact if anything it makes me want to smoke a cigarette", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "alexrtay") 
+ m.comments << Comment.create(comment_text: "Is this saying that if you smoke, you look gay, or if you don't smoke, you look gay. 'Cause I don't want to look like that guy", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "i_mason_storm_ya") 
+ m.comments << Comment.create(comment_text: "This makes me want to chain smoke 25 packs and pray to god that the cancer gets me ASAP", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "seandunn19") 
+ m.comments << Comment.create(comment_text: "Ew wtf", comment_date: DateTime.new(2017,4,18,0,0,0,"-07:00"), commentator_username: "cshelton33004") 
+m.save
+
+
+ #organic instagram
+ 
+ 
+m = Message.find(10498)
+ m.comments << Comment.create(comment_text: ";) ", comment_date: DateTime.new(2017,4,21,0,0,0,"-07:00"), commentator_username: "10sdab") 
+m.save
+
+m = Message.find(10553)
+ m.comments << Comment.create(comment_text: "This is nice :)", comment_date: DateTime.new(2017,4,25,0,0,0,"-07:00"), commentator_username: "cigarfanaticsworld") 
+m.save
+ 
+m = Message.find(10628)
+ m.comments << Comment.create(comment_text: "Awesome shots loving your gallery! #MrBrog", comment_date: DateTime.new(2017,4,30,0,0,0,"-07:00"), commentator_username: "mrbrog") 
+m.save
+
+m = Message.find(10723)
+ m.comments << Comment.create(comment_text: "Wow! I love this photo :D", comment_date: DateTime.new(2017,5,6,0,0,0,"-07:00"), commentator_username: "elfbreadshop") 
+m.save
+
+m = Message.find(10973)
+ m.comments << Comment.create(comment_text: "nice!! im sure you'd love some of our pieces", comment_date: DateTime.new(2017,5,31,0,0,0,"-07:00"), commentator_username: "lit.tire.glass") 
+m.save
+
+m = Message.find(11318)
+ m.comments << Comment.create(comment_text: "Follow me fams :)", comment_date: DateTime.new(2017,6,15,0,0,0,"-07:00"), commentator_username: "yung_drab") 
+m.save
+
+m = Message.find(11383)
+ m.comments << Comment.create(comment_text: "we all die anyway tho why not smoke if ya want", comment_date: DateTime.new(2017,6,19,0,0,0,"-07:00"), commentator_username: "tobiohoe") 
+m.save
+
+m = Message.find(11398)
+ m.comments << Comment.create(comment_text: "Enjoy! :) ", comment_date: DateTime.new(2017,6,21,0,0,0,"-07:00"), commentator_username: "valentiacigars") 
+m.save
+
+m = Message.find(11493)
+ m.comments << Comment.create(comment_text: "Robi wrażenie :) ", comment_date: DateTime.new(2017,6,27,0,0,0,"-07:00"), commentator_username: "vapymyflavorite") 
+m.save
+
+m = Message.find(11573)
+ m.comments << Comment.create(comment_text: "(100 emoji)", comment_date: DateTime.new(2017,7,2,0,0,0,"-07:00"), commentator_username: "flovapor") 
+m.save
+
+ 
+ 
+ 
+ 
