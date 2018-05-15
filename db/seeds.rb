67c998c120b3d6878d836e703c58b0418baf49ae
Alicia M <aflyingfemme@gmail.com>
2018-05-15 19:39:43 +0000

    Add seeds for instagram and twitter comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'csv'
 
-if Hashtag.count == 0
-  CSV.foreach("#{Rails.root}/db/seed_files/healthcare-hashtags-diseases.csv") do |row|
-    begin
-      Hashtag.create!(phrase: row[0])
-    rescue ActiveRecord::RecordInvalid => e
-      throw e if e.message != 'Validation failed: Phrase has already been taken'
-    end
+require 'axlsx'
+require 'csv'
+require 'net/http'
+require 'roo'
+
+comments = Comment.select{ |comment| comment.message.platform == :twitter || comment.message.platform == :instagram }
+comments.each{ |comment| comment.destroy }
+
+CSV.parse(Net::HTTP.get(URI.parse('https://s3-us-west-1.amazonaws.com/scctsi-tp-production/1-tcors/comments/instagram_comments.csv'))) do |row|
+  comment = Comment.new(message_id: row[0], comment_date: row[2], comment_text: row[3], commentator_username: row[4])
+  if !(row[0].nil?)
+    comment.save
+    message = Message.find(row[0]).comments << comment
+    message.save
+  end
+end
+
+message_id_sheet = Roo::Spreadsheet.open('https://s3-us-west-1.amazonaws.com/scctsi-tp-production/1-tcors/comments/message_ids_for_twitter_ad_comments.xlsx')
+message_ids = message_id_sheet.sheet(0)
+for index in (2..message_ids.last_row)
+  comment = Comment.new(message_id: message_ids.cell(index, 1), comment_date: message_ids.cell(index, 3), comment_text: message_ids.cell(index, 2), commentator_username: message_ids.cell(index, 4))
+  if !(comment.message_id.nil?)
+    comment.save
+    message = Message.find(message_id).comments << comment
+    message.save
   end
 end
