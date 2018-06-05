bcf93961ab1b657ade8d1ebe89619963e4fc44fb
Alicia M <aflyingfemme@gmail.com>
2018-06-05 18:52:24 +0000

    Delete job

--- app/jobs/aggregate_facebook_comments_job.rb
+++
@@ -1,7 +0,0 @@
-class AggregateFacebookCommentsJob < ActiveJob::Base
-  queue_as :default
- 
-  def perform(experiment)
-    FacebookCommentsAggregator(experiment.configure_settings.facebook_access_token)
-  end
-end
