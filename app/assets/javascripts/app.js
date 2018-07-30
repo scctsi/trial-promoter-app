3b06e2541859da5e6be1576ab637dc10a05ceae5
Praveen Angyan <angyan@usc.edu>
2018-07-30 11:56:15 -0700

    Remove FE/TFL specific code (used in TCORS experiment). (#463)
    * Fixed paginated tab (#457)
    
    * Add eager loading to experiments controller; test (#459)
    
    * Add eager load to experiments home view
    
    * Add number of days between posting
    
    * Delete hard code
    
    * Add ability to not use Click Meter
    
    * Fix tests
    
    * Fix Fresh Empire/This Free Life specific code
    
    * Fix FE/TFL specific code

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -437,25 +437,14 @@ $(document).ready(function() {
     return html;
   }
 
-  function getImagePoolInterfaceHtml(selectedImages, unselectedImages, messageContent, filenameStartswithRestriction, readonly) {
+  function getImagePoolInterfaceHtml(selectedImages, unselectedImages, messageContent, filenameStartswithRestriction, readonly = false) {
     var html = '<div class="ui segment">' + messageContent + '</div>';
     html += '<div class="ui segment filenames-list">Filenames: ';
     html += getFilenames(selectedImages) + '</div>';
 
-    if (filenameStartswithRestriction == 'tfl') {
-      html += '<h3 class="ui block header">Selected images from "This Free Life" campaign</h3>';
-    }
-    if (filenameStartswithRestriction == 'fe') {
-      html += '<h3 class="ui block header">Selected images from "Fresh Empire" campaign</h3>';
-    }
-    html += getImageCardsHtml(selectedImages, 'remove', filenameStartswithRestriction, readonly);
+    html += '<h3 class="ui block header">Selected images from this campaign</h3>';
 
-    if (filenameStartswithRestriction == 'tfl') {
-      html += '<h3 class="ui block header">Unselected images from "This Free Life" campaign</h3>';
-    }
-    if (filenameStartswithRestriction == 'fe') {
-      html += '<h3 class="ui block header">Unselected images from "Fresh Empire" campaign</h3>';
-    }
+    html += getImageCardsHtml(selectedImages, 'remove', filenameStartswithRestriction, readonly);
     html += getImageCardsHtml(unselectedImages, 'add', filenameStartswithRestriction, readonly);
 
     return html;
@@ -608,12 +597,6 @@ $(document).ready(function() {
     });
   }
   
-  
-
-  /* Under Construction */
-  /* Under Construction */
-  /* Under Construction */
-
   function setUpSaveNoteFormEvents() {
     $('.button.save-note').click(function(event){
       var $inputForm = $(this).parent();
