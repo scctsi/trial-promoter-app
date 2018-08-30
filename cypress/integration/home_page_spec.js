3d994c50dfc6cb1088a3fee159ab6826a5f318e6
yokuba <aflyingfemme@gmail.com>
2018-08-30 12:57:22 -0700

    Add localhost to test

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,7 +2,7 @@ describe('The Home Page', function() {
   it('successfully loads', function() {
     // there is a before action that creates a user for all tests
     // so this starts at the index page, rather than the login page
-    cy.visit('/')
+    cy.visit('localhost:3000/')
 
     cy.get('[data-method=delete]').should('contain', 'Logout')
 
@@ -13,7 +13,7 @@ describe('The Home Page', function() {
 describe('The new experiment page', function () {
   it('restricts a user from creating a new experiment', function () {
 
-    cy.visit('/experiments/new')
+    cy.visit('localhost:3000/experiments/new')
     cy.get('[data-cy=unauthorized]').should('contain', 'You are not authorized to perform this action.')
     cy.get('[data-cy=home]').should('contain', 'Home')
   })
